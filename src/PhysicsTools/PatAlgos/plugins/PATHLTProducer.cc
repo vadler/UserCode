92f5a1a4a8e12b169d0dad28119c5131b0b0c352
vadler <>
2008-03-11 13:52:32 +0000

    Added trigger names (in StringMap) to the event.

--- src/PhysicsTools/PatAlgos/plugins/PATHLTProducer.cc
+++ src/PhysicsTools/PatAlgos/plugins/PATHLTProducer.cc
@@ -1,5 +1,5 @@
 //
-// $Id: PATHLTProducer.cc,v 1.1 2008/03/06 17:50:39 vadler Exp $
+// $Id: PATHLTProducer.cc,v 1.1.2.1 2008/03/07 18:18:35 vadler Exp $
 //
 
 
@@ -12,6 +12,7 @@
 
 #include "FWCore/Framework/interface/TriggerNames.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/PatCandidates/interface/StringMap.h"
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
@@ -21,9 +22,11 @@ PATHLTProducer::PATHLTProducer( const ParameterSet& iConfig ) :
   // initialize
   triggerResults_ (iConfig.getParameter<InputTag>( "triggerResults" ) ),
   triggerName_    (iConfig.getParameter<string>  ( "triggerName" ) ),
-  filterName_     (iConfig.getParameter<InputTag>( "filterName" ) )
+  filterName_     (iConfig.getParameter<InputTag>( "filterName" ) ),
+  report_         (iConfig.getParameter<bool>    ( "reportVerbose" ) )
 {
   produces<reco::CandidateCollection>();
+  produces<StringMap>();
 }
 
 
@@ -35,40 +38,49 @@ PATHLTProducer::~PATHLTProducer()
 void PATHLTProducer::produce( Event& iEvent, const EventSetup& iSetup )
 {
   auto_ptr<reco::CandidateCollection> patHltCandidates( new reco::CandidateCollection );
+  auto_ptr<StringMap> patHltTriggerNames( new StringMap );
+  
   Handle<TriggerResults> triggerResults;
   iEvent.getByLabel( triggerResults_, triggerResults );
   TriggerNames triggerNames( *triggerResults );
+  
   unsigned int triggerIndex = triggerNames.triggerIndex( triggerName_ );
   if ( triggerIndex == triggerNames.size() ) {
-    LogWarning( "wrongTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " is not known in this event!";
+    if ( report_ ) LogWarning( "wrongTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " is not known in this event!";
   } else if ( ! triggerResults->wasrun( triggerIndex ) ) {
-    LogWarning( "notrunTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " was not run in this event!";
+    if ( report_ ) LogWarning( "notrunTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " was not run in this event!";
   } else if ( ! triggerResults->accept( triggerIndex ) ) {
-    LogWarning( "notacceptTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " did not accept this event!";
+    if ( report_ ) LogWarning( "notacceptTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " did not accept this event!";
   } else if (   triggerResults->error ( triggerIndex ) ) {
-    LogWarning( "errorTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " had an error in this event!";
+    if ( report_ ) LogWarning( "errorTriggerPath" ) << "PATHLTProducer: The trigger path " << triggerName_ << " had an error in this event!";
   } else {
     Handle<reco::HLTFilterObjectWithRefs> hltFilter;
     try { // In this case, we want to act differently compared to the usual behaviour on "ProductNotFound" exception thrown by Event::getByLabel.
       iEvent.getByLabel( filterName_, hltFilter );
       if ( triggerIndex != hltFilter->path() ) {
-        LogWarning( "wrongTriggerModule" ) << "PATHLTProducer: The filter module " << filterName_.label() << " does not belong to the trigger path " << triggerName_ << "!";
+        if ( report_ ) LogWarning( "wrongTriggerModule" ) << "PATHLTProducer: The filter module " << filterName_.label() << " does not belong to the trigger path " << triggerName_ << "!";
       } else {
+        // loop over trigger objects and store trigger candidates
         for ( unsigned int iTriggerObject = 0; iTriggerObject < hltFilter->size(); ++iTriggerObject ) {
           const reco::Candidate * patHltCandidate( &(hltFilter->at(iTriggerObject)) );
           auto_ptr<reco::Candidate> ptr( patHltCandidate->clone() );
-          patHltCandidates->push_back( ptr );        
+          patHltCandidates->push_back( ptr );
+          patHltTriggerNames->add( triggerName_, iTriggerObject );        
+          patHltTriggerNames->add( filterName_.label(), -iTriggerObject );        
         }  
       }
     } catch( Exception exc ) {
       if ( exc.codeToString( exc.categoryCode() ) == "ProductNotFound" ) {
-        LogWarning( "notpresentTriggerModule" ) << "PATHLTProducer: The filter module " << filterName_.label() << " is not present here!";
+//         if ( report_ ) LogWarning( "notpresentTriggerModule" ) << "PATHLTProducer: The filter module " << filterName_.label() << " is not present here!";
+        LogWarning( "notpresentTriggerModule" ) << "PATHLTProducer: The filter module " << filterName_.label() << " is not present here!"; // This should be reported in  a n y  case.
       } else {
         throw exc;
       }
     }
   }
+  
   iEvent.put( patHltCandidates );
+  iEvent.put( patHltTriggerNames );
 }
 
 
--- src/PhysicsTools/PatAlgos/plugins/PATL1Producer.cc
+++ src/PhysicsTools/PatAlgos/plugins/PATL1Producer.cc
@@ -1,5 +1,5 @@
 //
-// $Id: PATL1Producer.cc,v 1.1 2008/03/06 17:50:39 vadler Exp $
+// $Id: PATL1Producer.cc,v 1.1.2.1 2008/03/07 18:18:35 vadler Exp $
 //
 
 
@@ -8,6 +8,7 @@
 #include "DataFormats/L1Trigger/interface/L1ParticleMap.h"
 
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/PatCandidates/interface/StringMap.h"
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
@@ -18,9 +19,11 @@ PATL1Producer::PATL1Producer( const ParameterSet& iConfig ) :
   // initialize
   particleMaps_ (iConfig.getParameter<InputTag>( "particleMaps" ) ),
   triggerName_  (iConfig.getParameter<string>  ( "triggerName" ) ),
-  objectType_   (iConfig.getParameter<string>  ( "objectType" ) )
+  objectType_   (iConfig.getParameter<string>  ( "objectType" ) ),
+  report_       (iConfig.getParameter<bool>    ( "reportVerbose" ) )
 {
   produces<reco::CandidateCollection>();
+  produces<StringMap>();
 }
 
 
@@ -32,48 +35,62 @@ PATL1Producer::~PATL1Producer()
 void PATL1Producer::produce( Event& iEvent, const EventSetup& iSetup )
 {
   auto_ptr<reco::CandidateCollection> patL1Candidates( new reco::CandidateCollection );
+  auto_ptr<StringMap> patL1TriggerNames( new StringMap );
+  
   Handle<L1ParticleMapCollection> particleMaps;
   iEvent.getByLabel( particleMaps_, particleMaps );
+  
   const L1ParticleMap::L1TriggerType triggerType = L1ParticleMap::triggerType( triggerName_ );
   if ( triggerType == L1ParticleMap::kNumOfL1TriggerTypes ) {
-    LogWarning( "wrongL1TriggerName" ) << "PATL1Producer: The L1 trigger name " << triggerName_ << " is not known in this event!";
+    if ( report_ ) LogWarning( "wrongL1TriggerName" ) << "PATL1Producer: The L1 trigger name " << triggerName_ << " is not known in this event!";
   } else {
     const L1ParticleMap& particleMap = particleMaps->at( triggerType );
     if ( ! particleMap.triggerDecision() ) {
-      LogWarning( "notacceptL1Trigger" ) << "PATL1Producer: The L1 trigger " << triggerName_ << " did not accept this event!";
+      if ( report_ ) LogWarning( "notacceptL1Trigger" ) << "PATL1Producer: The L1 trigger " << triggerName_ << " did not accept this event!";
     } else {
       if ( objectType_ == "em" ) { // isolated or non-isolated (for electrons and photons)
         const L1EmParticleVectorRef& triggeredObjects = particleMap.emParticles();
-        for ( unsigned int iTriggeredObjects = 0; iTriggeredObjects < triggeredObjects.size(); ++iTriggeredObjects ) {
-          const reco::Candidate * patL1Candidate( triggeredObjects[ iTriggeredObjects ]->clone() );
+        // loop over L1 trigger objects and store L1 trigger candidates
+        for ( unsigned int iTriggerObject = 0; iTriggerObject < triggeredObjects.size(); ++iTriggerObject ) {
+          const reco::Candidate * patL1Candidate( triggeredObjects[ iTriggerObject ]->clone() );
           auto_ptr<reco::Candidate> ptr( patL1Candidate->clone() );
-          patL1Candidates->push_back( ptr );        
+          patL1Candidates->push_back( ptr );
+          patL1TriggerNames->add( triggerName_, iTriggerObject );
         }
       } else if ( objectType_ == "muon" ) {
         const L1MuonParticleVectorRef& triggeredObjects = particleMap.muonParticles();
-        for ( unsigned int iTriggeredObjects = 0; iTriggeredObjects < triggeredObjects.size(); ++iTriggeredObjects ) {
-          const reco::Candidate * patL1Candidate( triggeredObjects[ iTriggeredObjects ]->clone() );
+        // loop over L1 trigger objects and store L1 trigger candidates
+        for ( unsigned int iTriggerObject = 0; iTriggerObject < triggeredObjects.size(); ++iTriggerObject ) {
+          const reco::Candidate * patL1Candidate( triggeredObjects[ iTriggerObject ]->clone() );
           auto_ptr<reco::Candidate> ptr( patL1Candidate->clone() );
           patL1Candidates->push_back( ptr );        
+          patL1TriggerNames->add( triggerName_, iTriggerObject );
         }
       } else if ( objectType_ == "jet" ) { // central or forward (for jets) or tau
         const L1JetParticleVectorRef& triggeredObjects = particleMap.jetParticles();
-        for ( unsigned int iTriggeredObjects = 0; iTriggeredObjects < triggeredObjects.size(); ++iTriggeredObjects ) {
-          const reco::Candidate * patL1Candidate( triggeredObjects[ iTriggeredObjects ]->clone() );
+        // loop over L1 trigger objects and store L1 trigger candidates
+        for ( unsigned int iTriggerObject = 0; iTriggerObject < triggeredObjects.size(); ++iTriggerObject ) {
+          const reco::Candidate * patL1Candidate( triggeredObjects[ iTriggerObject ]->clone() );
           auto_ptr<reco::Candidate> ptr( patL1Candidate->clone() );
           patL1Candidates->push_back( ptr );        
+          patL1TriggerNames->add( triggerName_, iTriggerObject );
         }
       } else if ( objectType_ == "met" ) {
         const L1EtMissParticleRefProd& triggeredObject = particleMap.etMissParticle();
+        // store L1 trigger candidates
         const reco::Candidate * patL1Candidate( triggeredObject->clone() );
         auto_ptr<reco::Candidate> ptr( patL1Candidate->clone() );
         patL1Candidates->push_back( ptr );        
+        patL1TriggerNames->add( triggerName_, 0 );
       } else { // wrong input to configurable
-        LogWarning( "wrongL1Object" ) << "PATL1Producer: The L1 object type " << objectType_ << "does not exist!";
+//         if ( report_ ) LogWarning( "wrongL1Object" ) << "PATL1Producer: The L1 object type " << objectType_ << "does not exist!";
+        if ( report_ ) LogWarning( "wrongL1Object" ) << "PATL1Producer: The L1 object type " << objectType_ << "does not exist!"; // This should be reported in  a n y  case.
       }
     }
   }
+  
   iEvent.put( patL1Candidates );
+  iEvent.put( patL1TriggerNames );
 }
 
 
