cc3c804745d290583a2582a442e2da2bae95dcf6
vadler <>
2008-04-14 11:37:29 +0000

    Using new class TriggerPrimitive.

--- src/PhysicsTools/PatAlgos/plugins/PATHLTMatcher.cc
+++ src/PhysicsTools/PatAlgos/plugins/PATHLTMatcher.cc
@@ -1,5 +1,5 @@
 //
-// $Id: PATHLTMatcher.cc,v 1.1 2008/02/22 11:27:46 vadler Exp $
+// $Id$
 //
 
 
@@ -9,43 +9,31 @@
 #include "PhysicsTools/PatUtils/interface/PATMatchLessByDPt.h"
 
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/PatCandidates/interface/TriggerPrimitive.h"
 
 // Match by deltaR and deltaPt, ranking by deltaR (default)
 typedef pat::PATCandMatcher<
   reco::CandidateView,
-  reco::CandidateCollection,
+  pat::TriggerPrimitiveCollection,
   pat::PATHLTMatchSelector<reco::CandidateView::value_type,
-			   reco::CandidateCollection::value_type>,
+                           pat::TriggerPrimitiveCollection::value_type>,
   pat::PATMatchByDRDPt<reco::CandidateView::value_type,
-		       reco::CandidateCollection::value_type>
+                       pat::TriggerPrimitiveCollection::value_type>
 > PATHLTMatcher;
 
 // Alternative: match by deltaR and deltaPt, ranking by deltaPt
 typedef pat::PATCandMatcher<
   reco::CandidateView,
-  reco::CandidateCollection,
+  pat::TriggerPrimitiveCollection,
   pat::PATHLTMatchSelector<reco::CandidateView::value_type,
-			   reco::CandidateCollection::value_type>,
+                           pat::TriggerPrimitiveCollection::value_type>,
   pat::PATMatchByDRDPt<reco::CandidateView::value_type,
-		       reco::CandidateCollection::value_type>,
+                       pat::TriggerPrimitiveCollection::value_type>,
   pat::PATMatchLessByDPt<reco::CandidateView,
- 			 reco::CandidateCollection >
+                         pat::TriggerPrimitiveCollection >
 > PATHLTMatcherByPt;
-
-// Maybe, also a special treatment of jets is needed in the trigger matching!
-// // JET Match by deltaR, ranking by deltaR (default)
-// typedef pat::PATCandMatcher<
-//   reco::CandidateView,
-//   reco::GenJetCollection,
-//   pat::PATMatchSelector<reco::CandidateView::value_type,
-// 			reco::GenJetCollection::value_type>,
-//   pat::PATMatchByDR<reco::CandidateView::value_type,
-// 		    reco::CandidateView::value_type>
-// > PATHLTJetMatcher;
 
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE( PATHLTMatcher );
 DEFINE_FWK_MODULE( PATHLTMatcherByPt );
-// DEFINE_FWK_MODULE( PATHLTJetMatcher );
-
