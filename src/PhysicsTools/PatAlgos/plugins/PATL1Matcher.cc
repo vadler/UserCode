06a9dadb205490a6aa84cd609100b559d60eece0
vadler <>
2008-04-14 12:41:28 +0000

    Moved former changes to correct branch.

--- src/PhysicsTools/PatAlgos/plugins/PATL1Matcher.cc
+++ src/PhysicsTools/PatAlgos/plugins/PATL1Matcher.cc
@@ -9,32 +9,30 @@
 #include "PhysicsTools/PatUtils/interface/PATMatchLessByDPt.h"
 
 #include "DataFormats/Candidate/interface/Candidate.h"
-#include "DataFormats/PatCandidates/interface/TriggerPrimitive.h"
 
 // Match by deltaR and deltaPt, ranking by deltaR (default)
 typedef pat::PATCandMatcher<
   reco::CandidateView,
-  pat::TriggerPrimitiveCollection,
-  pat::PATL1MatchSelector< reco::CandidateView::value_type,
-                           pat::TriggerPrimitiveCollection::value_type >,
-  pat::PATMatchByDRDPt< reco::CandidateView::value_type,
-                        pat::TriggerPrimitiveCollection::value_type >
+  reco::CandidateCollection,
+  pat::PATL1MatchSelector<reco::CandidateView::value_type,
+			   reco::CandidateCollection::value_type>,
+  pat::PATMatchByDRDPt<reco::CandidateView::value_type,
+		       reco::CandidateCollection::value_type>
 > PATL1Matcher;
 
 // Alternative: match by deltaR and deltaPt, ranking by deltaPt
 typedef pat::PATCandMatcher<
   reco::CandidateView,
-  pat::TriggerPrimitiveCollection,
-  pat::PATL1MatchSelector< reco::CandidateView::value_type,
-                           pat::TriggerPrimitiveCollection::value_type>,
-  pat::PATMatchByDRDPt< reco::CandidateView::value_type,
-                        pat::TriggerPrimitiveCollection::value_type >,
-  pat::PATMatchLessByDPt< reco::CandidateView,
-                          pat::TriggerPrimitiveCollection >
+  reco::CandidateCollection,
+  pat::PATL1MatchSelector<reco::CandidateView::value_type,
+			   reco::CandidateCollection::value_type>,
+  pat::PATMatchByDRDPt<reco::CandidateView::value_type,
+		       reco::CandidateCollection::value_type>,
+  pat::PATMatchLessByDPt<reco::CandidateView,
+ 			 reco::CandidateCollection >
 > PATL1MatcherByPt;
 
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE( PATL1Matcher );
 DEFINE_FWK_MODULE( PATL1MatcherByPt );
-
