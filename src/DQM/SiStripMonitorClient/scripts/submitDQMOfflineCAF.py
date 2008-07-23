59f323f61844afc9d3794fc8f5e3fa301e45a927
vadler <>
2008-07-23 10:32:07 +0000

    Further automation.

--- src/DQM/SiStripMonitorClient/scripts/submitDQMOfflineCAF.py
+++ src/DQM/SiStripMonitorClient/scripts/submitDQMOfflineCAF.py
@@ -12,19 +12,24 @@ import time
 def WrongUsage():
   print '> submitDQMOfflineCAF.py > wrong usage'
   print '  requires arguments:'
+  print '  - action level (\'create\', \'submit\')'
   print '  - run number'
   print '  - split level'
-  print '  - action level (\'create\', \'submit\')'
+  print '  - apply HLT filters? (\'True\'/\'False\')'
   sys.exit(1)
 
-if len(sys.argv) != 4:
+if len(sys.argv) < 4:
   WrongUsage()
 
-actionLevel =     sys.argv[1]
-runNumber   =     sys.argv[2]
-splitLevel  = int(sys.argv[3])
+argAction    =     sys.argv[1]
+argRunNumber =     sys.argv[2]
+argSplit     = int(sys.argv[3])
+argFiltersOn = False
+if len(sys.argv) >= 5:
+  if sys.argv[4] == '1' or sys.argv[4] == 'True' or  sys.argv[4] == 'true':
+    argFiltersOn = True
 
-if actionLevel != 'submit' and actionLevel != 'create':
+if argAction != 'submit' and argAction != 'create':
   WrongUsage()
 
 # shell = os.path.basename(os.getenv('SHELL'))
@@ -33,81 +38,98 @@ if actionLevel != 'submit' and actionLevel != 'create':
 #   shellType = 'csh'
 # command = 'source ' + os.getenv('VO_CMS_SW_DIR') + '/cmsset_default.' + shellType + '; cmsenv'  
 # os.system(command) # still doesn't work
-# cmsswBase = os.getenv('CMSSW_BASE')
-currentDir  = os.getcwd()
-pathStrings = string.split(currentDir, '/src/') # assumes to be in _the_ release working area
-cmsswBase   = pathStrings[0]                    # assumes to be in _the_ release working area 
-packageDir  = os.path.split(pathStrings[1])[0]  # works only one level below the package
+# pathCmsswBase = os.getenv('CMSSW_BASE')
+pathCurrentDir        = os.getcwd()
+pathCurrentDirStrings = string.split(pathCurrentDir, '/src/') # assumes to be in _the_ release working area
+pathCmsswBase         = pathCurrentDirStrings[0]                    # assumes to be in _the_ release working area 
+pathCmsswPackage      = os.path.split(pathCurrentDirStrings[1])[0]  # works only one level below the package
   
-digits = 1
-if int(runNumber) >= 10:
-  digits = int(math.log10(int(runNumber))) + 1
-runName = 'R'
-for i in range(9-digits):
-  runName += '0'
-runName += runNumber
+nDigits = 1
+if int(argRunNumber) >= 10:
+  nDigits = int(math.log10(int(argRunNumber))) + 1
+nameRun = 'R'
+for iJob in range(9-nDigits):
+  nameRun += '0'
+nameRun += argRunNumber
 
-for root, dirs, files in os.walk(runName, topdown = False):
-  for name in files:
-    os.remove(os.path.join(root, name))
-  for name in dirs:
-    os.rmdir(os.path.join(root, name))
-os.rmdir(runName)
-os.mkdir(runName)
+if os.path.exists(nameRun):
+  for root, dirs, files in os.walk(nameRun, topdown = False):
+    for name in files:
+      os.remove(os.path.join(root, name))
+    for name in dirs:
+      os.rmdir(os.path.join(root, name))
+  os.rmdir(nameRun)
+os.mkdir(nameRun)
 
-dbsParams  = urllib.urlencode({'dbsInst': 'cms_dbs_prod_global', 'blockName': '*', 'dataset': '/Cosmics/CRUZET3_CRUZET3_V2P_v3/RECO', 'userMode': 'user', 'run': runNumber, 'what': 'cff'})
-dbsOutput  = urllib.urlopen("https://cmsweb.cern.ch/dbs_discovery/getLFN_txt", dbsParams)
-inputFiles = 0
-storePath  = '/store/data/CRUZET3/Cosmics/RECO/CRUZET3_V2P_v3'
-inputCff = file(runName + '/' + runName + '.cff','w')
-for inputLine in dbsOutput.readlines():
-  if inputLine.find(storePath) > -1:
-    inputFiles = inputFiles + 1
-    inputCff.write(inputLine)
-inputCff.close()
-inputFilesJob = int(inputFiles/splitLevel) + 1
-if inputFiles == inputFilesJob * (splitLevel-1):
-  splitLevel -= 1
-print '> submitDQMOfflineCAF.py > input files for run' + runNumber + ': ' + str(inputFiles)
+nInputFiles       = 0
+fileInputFilesCff = file(nameRun + '/' + nameRun + '.cff','w')
+dbsParams    = urllib.urlencode({'dbsInst': 'cms_dbs_prod_global', 'blockName': '*', 'dataset': '/Cosmics/CRUZET3_CRUZET3_V2P_v3/RECO', 'userMode': 'user', 'run': argRunNumber, 'what': 'cff'})
+dbsOutput    = urllib.urlopen("https://cmsweb.cern.ch/dbs_discovery/getLFN_txt", dbsParams)
+pathDbsStore = '/store/data/CRUZET3/Cosmics/RECO/CRUZET3_V2P_v3' # --> configurable (s)
+for iLine in dbsOutput.readlines():
+  if iLine.find(pathDbsStore) > -1:
+    nInputFiles += 1
+    fileInputFilesCff.write(iLine)
+fileInputFilesCff.close()
+if nInputFiles <= argSplit:
+  argSplit = nInputFiles
+if argSplit == 0:
+  argSplit = 1
+inputFilesJob = int(nInputFiles/argSplit) + 1
+if nInputFiles%argSplit == 0:
+  inputFilesJob -= 1
+if nInputFiles == inputFilesJob*(argSplit-1) and argSplit > 1:
+  argSplit -= 1
+print '> submitDQMOfflineCAF.py > input files for run ' + argRunNumber + ': ' + str(nInputFiles)
   
-readLines = 0
-inputCff = file(runName + '/' + runName + '.cff','r')
-inputLines = inputCff.readlines()
-for i in range(splitLevel):
-  digits = 1
-  if i >= 10:
-    digits = int(math.log10(i)) + 1
-  jobName = runName + "_"
-  for j in range(4-digits):
-    jobName += '0'
-  jobName += str(i)
-  jobDir   = runName + "/" + jobName
-  os.mkdir(jobDir)
-  os.chdir(jobDir)
-  inputCffNameJob = 'inputFiles.cff'
-  os.system('sed -e \"s#INCLUDE_DIRECTORY#' + pathStrings[1] + '/' + jobDir + '#g\" -e \"s#INPUT_FILES#' + pathStrings[1] + '/' + jobDir + '/' + inputCffNameJob + '#g\" ' + cmsswBase + '/src/DQM/SiStripMonitorClient/test/SiStripDQMOfflineGlobalRunCAF_template.cfg > SiStripDQMOfflineGlobalRunCAF.cfg')
-  inputCffJob = file(inputCffNameJob, 'w')
-  inputCffJob.write('  source = PoolSource {\n')
-  inputCffJob.write('    untracked vstring fileNames = {\n')
-  for j in range(readLines, min(readLines+inputFilesJob, inputFiles)):
-    thisLine    = string.split('      \'/store/data/' + string.split(inputLines[j], '/store/data/')[1], '.root\',')[0] + '.root\',\n'
-    actualLine  = thisLine
-    if (j+1)%inputFilesJob == 0 or readLines == inputFiles-1:
-      actualLine = string.split(thisLine, ',')[0] + '\n'
-    inputCffJob.write(actualLine)
-    readLines += 1
-  inputCffJob.write('    }\n')
-  inputCffJob.write('  }\n')
-  inputCffJob.close()
-  os.system('sed -e \"s#OUTPUT_DIRECTORY#/tmp/' + os.getenv('USER') + '/' + jobDir + '#g\" ' + cmsswBase + '/src/DQM/SiStripMonitorClient/data/SiStripDQMOfflineGlobalRunCAF_template.cff > SiStripDQMOfflineGlobalRunCAF.cff')
-  os.system('sed -e \"s#CMSSW_BASE#' + cmsswBase + '#g\" -e \"s#RUN_NAME#' + runName + '#g\" -e \"s#JOB_NAME#' + jobName + '#g\" -e \"s#CURRENT_DIR#' + currentDir + '#g\" ' + cmsswBase + '/src/DQM/SiStripMonitorClient/scripts/SiStripDQMOfflineCAF_template.job > SiStripDQMOfflineCAF.job')
+nLinesRead = 0
+fileInputFilesCff = file(nameRun + '/' + nameRun + '.cff', 'r')
+linesInput = fileInputFilesCff.readlines()
+nameMergeScript = 'merge' + nameRun + '.job'
+fileMergeScript = file(nameRun + '/' + nameMergeScript, 'w')
+fileMergeScript.write('hadd -f ' + os.getenv('HOME') + '/scratch0/cms/SiStripDQM/CMSSW_2_0_10/output/DQM_SiStrip' + nameRun + '_CAF-standAlone.root \\\n') # --> configurable
+for iJob in range(argSplit):
+  nDigits = 1
+  if iJob >= 10:
+    nDigits = int(math.log10(iJob)) + 1
+  nameJob = nameRun + "_"
+  for j in range(4-nDigits):
+    nameJob += '0'
+  nameJob += str(iJob)
+  pathJobDir   = nameRun + "/" + nameJob
+  os.mkdir(pathJobDir)
+  os.chdir(pathJobDir)
+  nameInputFilesJobCff = 'inputFiles.cff'
+  if argFiltersOn:
+    os.system('sed -e \"s#HLT_FILTER#    hltFilter,#g\" -e \"s#INCLUDE_DIRECTORY#' + pathCurrentDirStrings[1] + '/' + pathJobDir + '#g\" -e \"s#INPUT_FILES#' + pathCurrentDirStrings[1] + '/' + pathJobDir + '/' + nameInputFilesJobCff + '#g\" ' + pathCmsswBase + '/src/DQM/SiStripMonitorClient/test/SiStripDQMOfflineGlobalRunCAF_template.cfg > SiStripDQMOfflineGlobalRunCAF.cfg')
+  else:
+    os.system('sed -e \"s#HLT_FILTER#//     hltFilter,#g\" -e \"s#INCLUDE_DIRECTORY#' + pathCurrentDirStrings[1] + '/' + pathJobDir + '#g\" -e \"s#INPUT_FILES#' + pathCurrentDirStrings[1] + '/' + pathJobDir + '/' + nameInputFilesJobCff + '#g\" ' + pathCmsswBase + '/src/DQM/SiStripMonitorClient/test/SiStripDQMOfflineGlobalRunCAF_template.cfg > SiStripDQMOfflineGlobalRunCAF.cfg')
+  fileInputFilesJobCff = file(nameInputFilesJobCff, 'w')
+  fileInputFilesJobCff.write('  source = PoolSource {\n    untracked vstring fileNames = {\n')
+  for j in range(nLinesRead, min(nLinesRead+inputFilesJob, nInputFiles)):
+    line1  = string.split('      \'/store/data/' + string.split(linesInput[j], '/store/data/')[1], '.root\',')[0] + '.root\',\n'
+    line2  = line1
+    if (j+1)%inputFilesJob == 0 or nLinesRead == nInputFiles-1:
+      line2 = string.split(line1, ',')[0] + '\n'
+    fileInputFilesJobCff.write(line2)
+    nLinesRead += 1
+  fileInputFilesJobCff.write('    }\n  }\n')
+  fileInputFilesJobCff.close()
+  if nLinesRead == nInputFiles:
+    fileMergeScript.write('rfio:' + os.getenv('CASTOR_HOME') + '/cms/SiStripDQM/OfflineDQM/CAF/DQM_SiStrip_' + nameJob + '.root\n')
+  else:
+    fileMergeScript.write('rfio:' + os.getenv('CASTOR_HOME') + '/cms/SiStripDQM/OfflineDQM/CAF/DQM_SiStrip_' + nameJob + '.root \\\n')
+  os.system('sed -e \"s#OUTPUT_DIRECTORY#/tmp/' + os.getenv('USER') + '/' + pathJobDir + '#g\" ' + pathCmsswBase + '/src/DQM/SiStripMonitorClient/data/SiStripDQMOfflineGlobalRunCAF_template.cff > SiStripDQMOfflineGlobalRunCAF.cff')
+  os.system('sed -e \"s#CMSSW_BASE#' + pathCmsswBase + '#g\" -e \"s#RUN_NAME#' + nameRun + '#g\" -e \"s#JOB_NAME#' + nameJob + '#g\" -e \"s#CURRENT_DIR#' + pathCurrentDir + '#g\" ' + pathCmsswBase + '/src/DQM/SiStripMonitorClient/scripts/SiStripDQMOfflineCAF_template.job > SiStripDQMOfflineCAF.job')
   os.chmod('SiStripDQMOfflineCAF.job',484)
-  if actionLevel == 'submit':
+  if argAction == 'submit':
     print '> submitDQMOfflineCAF.py >'
     print '  ' + os.getcwd() + ' : bsub -q cmscaf SiStripDQMOfflineCAF.job'
     os.system('bsub -q cmscaf SiStripDQMOfflineCAF.job')
-  os.chdir(currentDir)
+  os.chdir(pathCurrentDir)
+fileMergeScript.close()
+os.chmod(nameRun + '/' + nameMergeScript,484)
 
-if actionLevel == 'submit':
-  sleep(5)
+if argAction == 'submit':
+  time.sleep(5)
   os.system('bjobs -q cmscaf')
