75ca05ca3c7c1a6d402c3b6cd8557513902be924
vadler <>
2008-10-21 13:21:47 +0000

    Clean-up.

--- src/DQM/SiStripMonitorClient/scripts/getRunInfo.py
+++ src/DQM/SiStripMonitorClient/scripts/getRunInfo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# $Id: getRunInfo.py,v 1.3.2.1 2008/09/16 17:18:46 vadler Exp $
+# $Id: getRunInfo.py,v 1.3.2.3 2008/10/21 13:13:36 vadler Exp $
 #
 
 ## CMSSW/DQM/SiStripMonitorClient/scripts/getRunInfo.py
@@ -21,49 +21,26 @@ import datetime
 
 LSTR_arguments = sys.argv[1:]
 # numbers
-TD_shiftUTC = datetime.timedelta(hours = 2) # positive for timezones with later time than UTC
-INT_offset  = 8
+INT_offset = 8
 # strings
-STR_SiStrip             = 'SIST'
-STR_good                = 'SS_GOOD'
-STR_htmlL1Key           = '&lt;b>L1&amp;nbsp;Key:&lt;/b>'
-STR_htmlHLTKey          = '&lt;b>HLT&amp;nbsp;Key:&lt;/b>'
-STR_wwwDBSData          = 'https://cmsweb.cern.ch/dbs_discovery/getData'
-STR_headDatasets        = 'datasets'
-STR_headFiles           = 'available data files'
-LSTR_summaryKeys        = ['BField', 'HLT Version', 'L1 Rate', 'HLT Rate', 'L1 Triggers', 'HLT Triggers', 'LHC Fill', 'LHC Energy', 'Initial Lumi', 'Ending Lumi', 'Run Lumi', 'Run Live Lumi']
-LSTR_summaryKeysTrigger = ['L1 Key', 'HLT Key']   
+STR_SiStrip      = 'SIST'
+STR_wwwDBSData   = 'https://cmsweb.cern.ch/dbs_discovery/getData'
+STR_headDatasets = 'datasets'
+STR_headFiles    = 'available data files'
 
 # Globals
 
 global Str_run
 global Dict_cmsmonRunRegistry
-global Dict_cmsmonSubsystems
-global Dict_cmsmonRunSummary
 global Dict_dbsDatasets
 global Dict_dbsEvents
-global Lstr_hltPaths
 # initialise
 Str_run                = sys.argv[1]
 Dict_cmsmonRunRegistry = {}
-Dict_cmsmonSubsystems  = {}
-Dict_cmsmonRunSummary  = {}
 Dict_dbsDatasets       = {}
 Dict_dbsEvents         = {}
-Lstr_hltPaths          = []
 
 ## FUNCTIONS
-
-## Function Func_Exit()
-#
-#  Exit after error
-def Func_Exit():
-  """  Function Func_Exit():
-  Exit after error
-  """
-  print '                  exit'
-  print
-  sys.exit(1)
 
 ## Func_GetHtmlTags(str_text)
 #
@@ -121,6 +98,37 @@ def Func_GetHtmlTagValueAttr(str_value, str_text):
   Gets the (last) attributes of a given HTML tag value from a string
   """
   return str_text.split('\">'+str_value+'<')[0].split('=\"')[-1]
+  
+## Func_FillInfoRunRegistry()
+#    
+# Retrieves run info from RunRegistry and fills it into containers
+def Func_FillInfoRunRegistry():
+  """ Func_FillInfoRunRegistry():
+  Retrieves run info from RunRegistry and fills it into containers
+  """  
+  str_cmsmonRunRegistry     = urllib.urlencode({'format':'xml', 'intpl':'xml', 'qtype':'RUN_NUMBER', 'sortname':'RUN_NUMBER'})
+  file_cmsmonRunRegistry    = urllib.urlopen("http://pccmsdqm04.cern.ch/runregistry/runregisterdata", str_cmsmonRunRegistry)
+  str_cmsmonRunRegistryLong = ''
+  for str_cmsmonRunRegistry in file_cmsmonRunRegistry.readlines():
+    str_cmsmonRunRegistryLong += str_cmsmonRunRegistry.splitlines()[0]
+  bool_foundRun = False
+  str_cmsmonRun = ''
+  for int_runIndex in range(1,int(str_cmsmonRunRegistryLong.split('<RUNS')[1].split('>')[0].split('total=\"')[1].split('\"')[0])):
+    str_cmsmonRun = Func_GetHtmlTagValue('RUN', str_cmsmonRunRegistryLong, int_runIndex)
+    if Func_GetHtmlTagValue('NUMBER', str_cmsmonRun) == Str_run:
+      bool_foundRun = True
+      break
+  if not bool_foundRun:
+    print '> getRunInfo.py > run ' + Str_run + ' not found in run registry'
+    return False
+  dict_cmsmonHtmlTags = Func_GetHtmlTags(str_cmsmonRun)
+  for str_cmsmonHtmlTag in dict_cmsmonHtmlTags.keys():
+    if dict_cmsmonHtmlTags[str_cmsmonHtmlTag] == False:
+      Dict_cmsmonRunRegistry[str_cmsmonHtmlTag] = Func_GetHtmlTagValue(str_cmsmonHtmlTag, str_cmsmonRun)
+  if Dict_cmsmonRunRegistry['SUBSYSTEMS'].find(STR_SiStrip) < 0:
+    print '> getRunInfo.py > SiStrip was not in this run'
+    return False
+  return True
  
 ## MAIN PROGRAM
 
@@ -130,32 +138,8 @@ print
 
 # Get run information from the web
 
-# Run registry
-# get run registry
-str_cmsmonRunRegistry     = urllib.urlencode({'format':'xml', 'intpl':'xml', 'qtype':'RUN_NUMBER', 'sortname':'RUN_NUMBER'})
-file_cmsmonRunRegistry    = urllib.urlopen("http://pccmsdqm04.cern.ch/runregistry/runregisterdata", str_cmsmonRunRegistry)
-lstr_cmsmonRunRegistry    = []
-str_cmsmonRunRegistryLong = ''
-for str_cmsmonRunRegistry in file_cmsmonRunRegistry.readlines():
-  lstr_cmsmonRunRegistry.append(str_cmsmonRunRegistry) # store run registry information
-  str_cmsmonRunRegistryLong += str_cmsmonRunRegistry.splitlines()[0]
-bool_foundRun = False
-str_cmsmonRun = ''
-for int_runIndex in range(1,int(str_cmsmonRunRegistryLong.split('<RUNS')[1].split('>')[0].split('total=\"')[1].split('\"')[0])):
-  str_cmsmonRun = Func_GetHtmlTagValue('RUN', str_cmsmonRunRegistryLong, int_runIndex)
-  if Func_GetHtmlTagValue('NUMBER', str_cmsmonRun) == Str_run:
-    bool_foundRun = True
-    break
-dict_cmsmonHtmlTags = Func_GetHtmlTags(str_cmsmonRun)
-for str_cmsmonHtmlTag in dict_cmsmonHtmlTags.keys():
-  if dict_cmsmonHtmlTags[str_cmsmonHtmlTag] == False:
-    Dict_cmsmonRunRegistry[str_cmsmonHtmlTag] = Func_GetHtmlTagValue(str_cmsmonHtmlTag, str_cmsmonRun)
-if ( not bool_foundRun ):
-  print '> getRunInfo.py > run ' + Str_run + ' not found in run registry'
-  Func_Exit()
-if Dict_cmsmonRunRegistry['SUBSYSTEMS'].find(STR_SiStrip) < 0:
-  print '> getRunInfo.py > SiStrip was not in this run'
-  Func_Exit()
+# get run RunRegistry entries
+bool_runRegistry = Func_FillInfoRunRegistry()
 
 # get run DBS entries
 str_dbsRuns      = urllib.urlencode({'ajax':'0', '_idx':'0', 'pagerStep':'0', 'userMode':'user', 'release':'Any', 'tier':'Any', 'dbsInst':'cms_dbs_caf_analysis_01', 'primType':'Any', 'primD':'Any', 'minRun':Str_run, 'maxRun':Str_run})
@@ -188,44 +172,47 @@ for str_dbsDatasets in lstr_dbsDatasets:
 # Print information
 
 # from run registry
-print '> getRunInfo.py > * information from run registry *'
-print
-if 'GLOBAL_NAME' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > global name                  : ' + Dict_cmsmonRunRegistry['GLOBAL_NAME']
-if 'STATUS' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > status                       : ' + Dict_cmsmonRunRegistry['STATUS']
-if 'IN_DBS' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > in DBS                       : ' + Dict_cmsmonRunRegistry['IN_DBS']
-if 'SUBSYSTEMS' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > subsystems                   : ' + Dict_cmsmonRunRegistry['SUBSYSTEMS']
-if 'EVENTS' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > # of triggers                : ' + Dict_cmsmonRunRegistry['EVENTS']
-if 'START_TIME' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > start time (local)           : ' + Dict_cmsmonRunRegistry['START_TIME']
-if 'END_TIME' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > end time (local)             : ' + Dict_cmsmonRunRegistry['END_TIME']
-if 'L1KEY' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > L1 key                       : ' + Dict_cmsmonRunRegistry['L1KEY']
-if 'HLTKEY' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > HLT key                      : ' + Dict_cmsmonRunRegistry['HLTKEY']
-if 'L1SOURCES' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > L1 sources                   : ' + Dict_cmsmonRunRegistry['L1SOURCES']
-# if 'RUN_RATE' in Dict_cmsmonRunRegistry:
-#   print '> getRunInfo.py > event rate                   : ' + Dict_cmsmonRunRegistry['RUN_RATE'] + ' Hz'
-if 'STOP_REASON' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > stop reason                  : ' + Dict_cmsmonRunRegistry['STOP_REASON']
-if 'SHIFTER' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > DQM shifter                  : ' + Dict_cmsmonRunRegistry['SHIFTER']
-if 'CREATE_USER' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > entry created by             : ' + Dict_cmsmonRunRegistry['CREATE_USER']
-if 'CREATE_TIME' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > entry creation time          : ' + Dict_cmsmonRunRegistry['CREATE_TIME']
-if 'ONLINE_COMMENT' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > DQM online shifter\'s comment : ' + Dict_cmsmonRunRegistry['ONLINE_COMMENT']
-if 'OFFLINE_COMMENT' in Dict_cmsmonRunRegistry:
-  print '> getRunInfo.py > DQM offline shifter\'s comment: ' + Dict_cmsmonRunRegistry['OFFLINE_COMMENT']
-print
+if bool_runRegistry:
+  print
+  print '> getRunInfo.py > * information from run registry *'
+  print
+  if 'GLOBAL_NAME' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > global name                  : ' + Dict_cmsmonRunRegistry['GLOBAL_NAME']
+  if 'STATUS' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > status                       : ' + Dict_cmsmonRunRegistry['STATUS']
+  if 'IN_DBS' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > in DBS                       : ' + Dict_cmsmonRunRegistry['IN_DBS']
+  if 'SUBSYSTEMS' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > subsystems                   : ' + Dict_cmsmonRunRegistry['SUBSYSTEMS']
+  if 'EVENTS' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > # of triggers                : ' + Dict_cmsmonRunRegistry['EVENTS']
+  if 'START_TIME' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > start time (local)           : ' + Dict_cmsmonRunRegistry['START_TIME']
+  if 'END_TIME' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > end time (local)             : ' + Dict_cmsmonRunRegistry['END_TIME']
+  if 'L1KEY' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > L1 key                       : ' + Dict_cmsmonRunRegistry['L1KEY']
+  if 'HLTKEY' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > HLT key                      : ' + Dict_cmsmonRunRegistry['HLTKEY']
+  if 'L1SOURCES' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > L1 sources                   : ' + Dict_cmsmonRunRegistry['L1SOURCES']
+#   if 'RUN_RATE' in Dict_cmsmonRunRegistry:
+#     print '> getRunInfo.py > event rate                   : ' + Dict_cmsmonRunRegistry['RUN_RATE'] + ' Hz'
+  if 'STOP_REASON' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > stop reason                  : ' + Dict_cmsmonRunRegistry['STOP_REASON']
+  if 'SHIFTER' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > DQM shifter                  : ' + Dict_cmsmonRunRegistry['SHIFTER']
+  if 'CREATE_USER' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > entry created by             : ' + Dict_cmsmonRunRegistry['CREATE_USER']
+  if 'CREATE_TIME' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > entry creation time          : ' + Dict_cmsmonRunRegistry['CREATE_TIME']
+  if 'ONLINE_COMMENT' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > DQM online shifter\'s comment : ' + Dict_cmsmonRunRegistry['ONLINE_COMMENT']
+  if 'OFFLINE_COMMENT' in Dict_cmsmonRunRegistry:
+    print '> getRunInfo.py > DQM offline shifter\'s comment: ' + Dict_cmsmonRunRegistry['OFFLINE_COMMENT']
+
 # from DBS
+print
 print '> getRunInfo.py > * information from DBS *'
 print
 str_print = '> getRunInfo.py > ' + STR_headDatasets
