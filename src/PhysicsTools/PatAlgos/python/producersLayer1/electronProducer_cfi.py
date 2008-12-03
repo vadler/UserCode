223ffd374ffe2ceeabc9cc58d50996c436f3a330
vadler <>
2008-12-03 11:02:44 +0000

    Keeping up-to-date.

--- src/PhysicsTools/PatAlgos/python/producersLayer1/electronProducer_cfi.py
+++ src/PhysicsTools/PatAlgos/python/producersLayer1/electronProducer_cfi.py
@@ -1,10 +1,6 @@
-# The following comments couldn't be translated into the new config version:
-
-# Minimum Tk Pt
-# 
-# Endcaps
-# 
 import FWCore.ParameterSet.Config as cms
+
+from RecoEgamma.EgammaIsolationAlgos.eleIsoFromDepsModules_cff import eleIsoFromDepsEcalFromClusts,eleIsoFromDepsHcalFromTowers,eleIsoFromDepsTk
 
 allLayer1Electrons = cms.EDProducer("PATElectronProducer",
     # General configurables
@@ -44,20 +40,25 @@ allLayer1Electrons = cms.EDProducer("PATElectronProducer",
             # source IsoDeposit
             src = cms.InputTag("layer0ElectronIsolations","eleIsoDepositTk"),
             # parameters to compute isolation (Egamma POG defaults)
-            deltaR = cms.double(0.3),
-            vetos = cms.vstring('0.015', # inner radius veto cone
-                'Threshold(1.0)'),       # threshold on individual track pt
-            skipDefaultVeto = cms.bool(True),
+            vetos  = eleIsoFromDepsTk.deposits[0].vetos,
+            deltaR = eleIsoFromDepsTk.deposits[0].deltaR,
+            skipDefaultVeto = cms.bool(True), # This overrides previous settings
+#           # Or set your own vetos...
+#            deltaR = cms.double(0.3),
+#            vetos = cms.vstring('0.015', # inner radius veto cone
+#                'Threshold(1.0)'),       # threshold on individual track pt
         ),
         ecal = cms.PSet(
             # source IsoDeposit
-            #src = cms.InputTag("layer0ElectronIsolations","eleIsoDepositEcalFromHits"),
             src = cms.InputTag("layer0ElectronIsolations","eleIsoDepositEcalFromClusts"),
             # parameters to compute isolation (Egamma POG defaults)
-            deltaR = cms.double(0.4),
-            vetos = cms.vstring('EcalBarrel:0.040', 'EcalBarrel:RectangularEtaPhiVeto(-0.01,0.01,-0.5,0.5)',  # Barrel (|eta| < 1.479)
-                                'EcalEndcaps:0.070','EcalEndcaps:RectangularEtaPhiVeto(-0.02,0.02,-0.5,0.5)'),
-            skipDefaultVeto = cms.bool(True),
+            vetos  = eleIsoFromDepsEcalFromClusts.deposits[0].vetos,
+            deltaR = eleIsoFromDepsEcalFromClusts.deposits[0].deltaR,
+            skipDefaultVeto = cms.bool(True), # This overrides previous settings
+#           # Or set your own vetos...
+#            deltaR = cms.double(0.4),
+#            vetos = cms.vstring('EcalBarrel:0.040', 'EcalBarrel:RectangularEtaPhiVeto(-0.01,0.01,-0.5,0.5)',  # Barrel (|eta| < 1.479)
+#                                'EcalEndcaps:0.070','EcalEndcaps:RectangularEtaPhiVeto(-0.02,0.02,-0.5,0.5)'),
         ),
         ## other option, using eleIsoDepositEcalSCVetoFromClust (see also recoLayer0/electronIsolation_cff.py)
         #PSet ecal = cms.PSet( 
@@ -68,11 +69,13 @@ allLayer1Electrons = cms.EDProducer("PATElectronProducer",
         #),
         hcal = cms.PSet(
             # source IsoDeposit
-            #src = cms.InputTag("layer0ElectronIsolations","eleIsoDepositHcalFromHits"),
             src = cms.InputTag("layer0ElectronIsolations","eleIsoDepositHcalFromTowers"),
             # parameters to compute isolation (Egamma POG defaults)
-            deltaR = cms.double(0.4),
-            skipDefaultVeto = cms.bool(True),
+            vetos  = eleIsoFromDepsHcalFromTowers.deposits[0].vetos,
+            deltaR = eleIsoFromDepsHcalFromTowers.deposits[0].deltaR,
+            skipDefaultVeto = cms.bool(True),  # This overrides previous settings
+#           # Or set your own vetos...
+#            deltaR = cms.double(0.4),
         ),
         user = cms.VPSet(),
     ),
