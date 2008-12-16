00e463d2ef5e8bb32c1c513d1afb600255ca0ebf
vadler <>
2008-12-16 18:14:07 +0000

    The PAT trigger information seems to work nowll */*/*/*~ */*/*/*/*~!

--- src/PhysicsTools/PatAlgos/python/triggerLayer0/triggerProducer_cfi.py
+++ src/PhysicsTools/PatAlgos/python/triggerLayer0/triggerProducer_cfi.py
@@ -3,9 +3,6 @@ import FWCore.ParameterSet.Config as cms
 patTrigger = cms.EDProducer( "PATTriggerProducer",
     processName      = cms.string( 'HLT' ),
     triggerResults   = cms.InputTag( "TriggerResults" ),
-    triggerEvent     = cms.InputTag( "hltTriggerSummaryAOD" ),
-    instancePaths    = cms.string( 'paths' ),
-    instanceFilters  = cms.string( 'filters' ),
-    instanceObjects  = cms.string( 'objects' )
+    triggerEvent     = cms.InputTag( "hltTriggerSummaryAOD" )
 )
 
