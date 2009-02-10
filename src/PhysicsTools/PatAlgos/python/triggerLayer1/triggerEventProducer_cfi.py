3d61b7160a1b56bcccebb652c468ad32c7f2437b
vadler <>
2009-02-10 18:44:27 +0000

    This commit was manufactured by cvs2svn to create tag 'V03-01-06'.
    Sprout from PAT-B2_2_X 2009-02-10 18:44:25 UTC vadler 'Catching up with with recent developments.'
    Delete:
        src/PhysicsTools/PatAlgos/BuildFile
        src/PhysicsTools/PatAlgos/plugins/BuildFile
        src/PhysicsTools/PatAlgos/plugins/PATElectronProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATElectronProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATJetProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATJetProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATMETProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATMETProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATMuonProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATMuonProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATPhotonProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATPhotonProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATTauProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATTauProducer.cc.new
        src/PhysicsTools/PatAlgos/plugins/PATTauProducer.cc.old
        src/PhysicsTools/PatAlgos/plugins/PATTauProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATTauProducer.h.new
        src/PhysicsTools/PatAlgos/plugins/PATTauProducer.h.old
        src/PhysicsTools/PatAlgos/plugins/PATTrigMatchSelector.h
        src/PhysicsTools/PatAlgos/plugins/PATTrigMatcher.cc
        src/PhysicsTools/PatAlgos/plugins/PATTrigProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATTrigProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATTriggerEventProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATTriggerEventProducer.h
        src/PhysicsTools/PatAlgos/plugins/PATTriggerProducer.cc
        src/PhysicsTools/PatAlgos/plugins/PATTriggerProducer.h
        src/PhysicsTools/PatAlgos/plugins/myTriggerTest.cc
        src/PhysicsTools/PatAlgos/plugins/myTriggerTest.h
        src/PhysicsTools/PatAlgos/python/patEventContent_cff.py
        src/PhysicsTools/PatAlgos/python/patSequences_cff.py
        src/PhysicsTools/PatAlgos/python/producersLayer1/electronProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/producersLayer1/jetProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/producersLayer1/metProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/producersLayer1/muonProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/producersLayer1/photonProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/producersLayer1/tauProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/producersLayer1/tauProducer_cfi.py.new
        src/PhysicsTools/PatAlgos/python/producersLayer1/tauProducer_cfi.py.old
        src/PhysicsTools/PatAlgos/python/tools/tauTools.py
        src/PhysicsTools/PatAlgos/python/triggerLayer0/patTrigMatcher_cfi.py
        src/PhysicsTools/PatAlgos/python/triggerLayer0/patTrigProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/triggerLayer0/trigMatchSequences_cff.py
        src/PhysicsTools/PatAlgos/python/triggerLayer1/triggerEventProducer_cfi.py
        src/PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cff.py
        src/PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
        src/PhysicsTools/PatAlgos/tags.txt
        src/PhysicsTools/PatAlgos/tags.txt.new
        src/PhysicsTools/PatAlgos/tags.txt.old
        src/PhysicsTools/PatAlgos/test/myTriggerTest_cfg.py
        src/PhysicsTools/PatAlgos/test/my_PatLayer1_fromAOD_full_cfg.py
        src/PhysicsTools/PatAlgos/test/my_hltAnalysis_cfg.py
        src/PhysicsTools/PatAlgos/test/patLayer1_fromAOD_triggerInfo_full.cfg.py

--- src/PhysicsTools/PatAlgos/python/triggerLayer1/triggerEventProducer_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patTriggerEvent = cms.EDProducer( "PATTriggerEventProducer",
-    processName    = cms.string( 'HLT' ),
-    triggerResults = cms.InputTag( "TriggerResults" ),
-    triggerEvent   = cms.InputTag( "hltTriggerSummaryAOD" ),
-    layer0Trigger  = cms.InputTag( "patTrigger" )
-)
-
--- src/PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patTrigger = cms.EDProducer( "PATTriggerProducer",
-    processName      = cms.string( 'HLT' ),
-    triggerResults   = cms.InputTag( "TriggerResults" ),
-    triggerEvent     = cms.InputTag( "hltTriggerSummaryAOD" )
-)
-
