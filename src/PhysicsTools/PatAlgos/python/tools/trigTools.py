123929ab8663f7e36f075019d588cb8c7ae0b08b
vadler <>
2009-04-23 15:30:21 +0000

    Added optional parameter 'dotrigMatch' to jetTools/addJetCollection.

--- src/PhysicsTools/PatAlgos/python/tools/trigTools.py
+++ src/PhysicsTools/PatAlgos/python/tools/trigTools.py
@@ -2,14 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 def switchOffTriggerMatchingOld( process ):
     """ Disables old style trigger matching in PAT  """
-    process.patDefaultSequence.remove( process.patTrigMatch )
     process.allLayer1Electrons.addTrigMatch = False
     process.allLayer1Muons.addTrigMatch     = False
     process.allLayer1Jets.addTrigMatch      = False
     process.allLayer1Photons.addTrigMatch   = False
     process.allLayer1Taus.addTrigMatch      = False
     process.layer1METs.addTrigMatch         = False
-    
+#     process.patDefaultSequence.remove( process.patTrigMatch )
+
 # for (temporary) backwards compatibility
 def switchTriggerOff( process ):
     switchOffTriggerMatchingOld( process )
