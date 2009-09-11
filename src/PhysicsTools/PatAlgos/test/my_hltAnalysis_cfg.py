1039bd0f18f11f06c799ad4074e4ea9d341b6c50
vadler <>
2009-09-11 09:02:38 +0000

    Switching to CMSSW_3_3_0_pre3.

--- src/PhysicsTools/PatAlgos/test/my_hltAnalysis_cfg.py
+++ src/PhysicsTools/PatAlgos/test/my_hltAnalysis_cfg.py
@@ -5,11 +5,11 @@ process = cms.Process( "HLTPROV" )
 # source
 process.source = cms.Source("PoolSource", 
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/CC80B73A-CA57-DE11-BC2F-000423D99896.root'
+        '/store/relval/CMSSW_3_3_0_pre2/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V7-v1/0002/BA958CA5-B19B-DE11-90C6-0018F3D0961A.root'
     )
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32( 1 )
+    input = cms.untracked.int32( 10 )
 )
 
 # HLT analyzers
