79a07797bbbb72d35e605a3019ea14f4e7cf4224
vadler <>
2009-09-28 18:53:52 +0000

    Switching to CMSSW_3_3_0_pre5, including bug fix in PhysicsTools/PatAlgos/python/producersLayer1/jetProducer_cfi.py.

--- src/PhysicsTools/PatAlgos/test/my_PatLayer1_fromAOD_full_cfg.py
+++ src/PhysicsTools/PatAlgos/test/my_PatLayer1_fromAOD_full_cfg.py
@@ -1,9 +1,9 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
-# process.GlobalTag.globaltag =  ...
-# process.source.fileNames    = [ ... ]
+process.GlobalTag.globaltag = 'STARTUP31X_V7::All'
+process.source.fileNames    = [ "/store/relval/CMSSW_3_3_0_pre5/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V7-v1/0003/00D6950D-0FAB-DE11-A478-001D09F29114.root" ]
 # process.maxEvents.input     = ...
 # process.out.outputCommands  = [ ... ]
-process.out.fileName        = '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_3_0_pre4/output/my_PatLayer1_fromAOD_full.root'
+process.out.fileName        = '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_3_0_pre5/output/my_PatLayer1_fromAOD_full.root'
 # process.options.wantSummary = False
 
 # # memory check
--- src/PhysicsTools/PatAlgos/test/my_hltAnalysis_cfg.py
+++ src/PhysicsTools/PatAlgos/test/my_hltAnalysis_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process( "HLTPROV" )
 # source
 process.source = cms.Source("PoolSource", 
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_3_0_pre2/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V7-v1/0002/BA958CA5-B19B-DE11-90C6-0018F3D0961A.root'
+        '/store/relval/CMSSW_3_3_0_pre5/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V7-v1/0003/00D6950D-0FAB-DE11-A478-001D09F29114.root'
     )
 )
 process.maxEvents = cms.untracked.PSet(
