77eb07c6b4e65a27f723e2edda7eb35e2854c6fd
vadler <>
2009-10-12 14:00:22 +0000

    Updating input and GT.

--- src/PhysicsTools/PatAlgos/test/myHltAnalysis_cfg.py.old
+++ src/PhysicsTools/PatAlgos/test/myHltAnalysis_cfg.py.old
@@ -5,7 +5,7 @@ process = cms.Process( "HLTPROV" )
 # source
 process.source = cms.Source("PoolSource", 
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_3_0_pre6/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V8-v1/0005/9EA6A564-FCB0-DE11-A4D1-001D09F29849.root'
+        '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V8-v1/0007/BAB355DB-DFB4-DE11-AD73-0019B9F72F97.root'
     )
 )
 process.maxEvents = cms.untracked.PSet(
--- src/PhysicsTools/PatAlgos/test/myPatLayer1_fromAOD_full_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myPatLayer1_fromAOD_full_cfg.py
@@ -1,6 +1,6 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
-process.GlobalTag.globaltag = 'STARTUP31X_V6::All'
-process.source.fileNames    = [ '/store/relval/CMSSW_3_3_0_pre6/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V8-v1/0005/9EA6A564-FCB0-DE11-A4D1-001D09F29849.root' ]
+process.GlobalTag.globaltag = 'STARTUP31X_V8::All'
+process.source.fileNames    = [ '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V8-v1/0007/BAB355DB-DFB4-DE11-AD73-0019B9F72F97.root' ]
 # process.maxEvents.input     = ...
 # process.out.outputCommands  = [ ... ]
 process.out.fileName        = '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_4_0_pre1/output/myPatLayer1_fromAOD_full.root'
