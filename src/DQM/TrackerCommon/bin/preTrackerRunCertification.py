e4a01a617cf45afb0da291426c4a654c6bb70100
vadler <>
2009-11-23 18:30:37 +0000

    Added options as arguments to script call.

--- src/DQM/TrackerCommon/bin/preTrackerRunCertification.py
+++ src/DQM/TrackerCommon/bin/preTrackerRunCertification.py
@@ -5,8 +5,22 @@
 import sys
 import xmlrpclib
 
-server = xmlrpclib.ServerProxy( 'http://pccmsdqm04.cern.ch/runregistry/xmlrpc' )
-data = server.DataExporter.export( 'GLOBAL', 'xml_all', {} )
-file = open( sys.argv[1], 'w' )
+# Argument handling (very simple)
+dArguments = { '-s': 'http://pccmsdqm04.cern.ch/runregistry/xmlrpc', # RunRegistry API proxy server
+               '-w': 'GLOBAL'                                      , # workspace
+               '-t': 'xml_all'                                     , # output format type
+               '-f': 'runRegistry.xml'                             } # output file
+iArgument  = 0
+for argument in sys.argv[ 1:-1 ]:
+  iArgument = iArgument + 1
+  if argument in dArguments.keys():
+    del dArguments[ argument ]
+    if not sys.argv[ iArgument + 1 ] in dArguments.keys():
+      dArguments[ argument ] = sys.argv[ iArgument + 1 ]
+
+# Data extraction and local storage
+server = xmlrpclib.ServerProxy( dArguments[ '-s' ] )                            # initialise API access to defined RunRegistry proxy
+data = server.DataExporter.export( dArguments[ '-w' ], dArguments[ '-t' ], {} ) # get data according to defined workspave and output format type
+file = open( dArguments[ '-f' ], 'w' )                                          # open defined output file in (over-)write mode
 file.write( data )
 file.close()
