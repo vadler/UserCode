eb1b31ab08b20edd60b810146da17511ab0536dd
vadler <>
2010-01-18 13:51:12 +0000

    Switching to CMSSW_3_5_0_pre3.

--- src/PhysicsTools/PatAlgos/test/myTriggerTest_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myTriggerTest_cfg.py
@@ -9,7 +9,7 @@ process.options = cms.untracked.PSet(
 
 process.source = cms.Source( "PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_5_X_2010-01-11-0300/output/myPatLayer1_fromAOD_full.root'
+        'file:/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_5_0_pre3/output/myPatLayer1_fromAOD_full.root'
     )
 )
 process.maxEvents = cms.untracked.PSet(
@@ -17,7 +17,7 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.TFileService = cms.Service( "TFileService",
-    fileName = cms.string( '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_5_X_2010-01-11-0300/output/myTriggerTest.root' )
+    fileName = cms.string( '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_5_0_pre3/output/myTriggerTest.root' )
 )
 
 process.triggerTest = cms.EDAnalyzer( "myTriggerTest",
