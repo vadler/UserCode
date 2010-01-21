85690e6e995b8e1e40e50948c8232d33109fce2d
vadler <>
2010-01-21 22:28:16 +0000

    Error checking logics revisited.

--- src/DQM/SiStripCommon/python/SiStripTriggerHelper_cff.py
+++ src/DQM/SiStripCommon/python/SiStripTriggerHelper_cff.py
@@ -14,21 +14,21 @@ import FWCore.ParameterSet.Config as cms
 SiStripHltFilter_SiStripMonitorHardware = cms.PSet(
     hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
     hltPaths    = cms.vstring(),
-    andOr       = cms.bool( False ), # "False": AND, "True": OR
+    andOr       = cms.bool( False ),
     errorReply  = cms.bool( True )
 )
 
 SiStripHltFilter_SiStripMonitorDigi = cms.PSet(
     hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
     hltPaths    = cms.vstring(),
-    andOr       = cms.bool( False ), # "False": AND, "True": OR
+    andOr       = cms.bool( False ),
     errorReply  = cms.bool( True )
 )
 
 SiStripHltFilter_SiStripMonitorCluster = cms.PSet(
     hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
     hltPaths    = cms.vstring(),
-    andOr       = cms.bool( False ), # "False": AND, "True": OR
+    andOr       = cms.bool( False ),
     errorReply  = cms.bool( True )
 )
 
@@ -37,7 +37,7 @@ SiStripHltFilter_SiStripMonitorTrack = cms.PSet(
     hltPaths    = cms.vstring( 'HLT_PhysicsDeclared'
                              , 'HLT_ZeroBias1kHz' # DEBUG
                              ),
-    andOr       = cms.bool( False ), # "False": AND, "True": OR
+    andOr       = cms.bool( False ),
     errorReply  = cms.bool( True )
 )
 
@@ -45,7 +45,7 @@ SiStripHltFilter_TrackerMonitorTrack = cms.PSet(
     hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
     hltPaths    = cms.vstring( 'HLT_PhysicsDeclared'
                              ),
-    andOr       = cms.bool( False ), # "False": AND, "True": OR
+    andOr       = cms.bool( False ),
     errorReply  = cms.bool( True )
 )
 
@@ -53,6 +53,6 @@ SiStripHltFilter_TrackingMonitor = cms.PSet(
     hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
     hltPaths    = cms.vstring( 'HLT_PhysicsDeclared'
                              ),
-    andOr       = cms.bool( False ), # "False": AND, "True": OR
+    andOr       = cms.bool( False ),
     errorReply  = cms.bool( True )
 )
--- src/DQM/SiStripCommon/src/SiStripTriggerHelper.cc
+++ src/DQM/SiStripCommon/src/SiStripTriggerHelper.cc
@@ -17,40 +17,42 @@ using namespace edm;
 bool SiStripTriggerHelper::accept( const Event & event, const ParameterSet & config )
 {
 
-  /// Configuration parameter tags
+  // Configuration parameter tags
   const string hltInputTagConfig( "hltInputTag" );
   const string hltPathConfig( "hltPaths" );
   const string andOrConfig( "andOr" );
   const string errorReplyConfig( "errorReply" );
 
-  /// Getting the desired reply in case of error from the configuration
-  errorReply_ = config.getParameter< bool >( errorReplyConfig );
-
-  /// Getting the TriggerResults InputTag from the configuration
+  // Getting the TriggerResults InputTag from the configuration
   // If an according InputTag is found in the configuration, it is expected to be meaningful and correct.
-  if ( ! config.exists( hltInputTagConfig ) ) return errorReply_;
+  // If not, the filter is switched off.
+  if ( ! config.exists( hltInputTagConfig ) ) return true;
   hltInputTag_ = config.getParameter< InputTag >( hltInputTagConfig );
+
+  // Getting the HLT path name of question from the configuration
+  // An empty configuration parameter acts also as switch.
+  hltPathNames_ = config.getParameter< vector< string > >( hltPathConfig );
+  if ( hltPathNames_.empty() ) return true;
+
+  // Getting remaining configuration parameters
+  errorReply_ = config.getParameter< bool >( errorReplyConfig );
+  andOr_ = config.getParameter< bool >( andOrConfig );
+
+  // Checking the TriggerResults InputTag
   // The process name has to be given.
   if ( hltInputTag_.process().size() == 0 ) {
     LogError( "hltProcess" ) << "HLT TriggerResults InputTag " << hltInputTag_.encode() << " specifies no process";
     return errorReply_;
   }
 
-  /// Getting the HLT path name of question from the configuration
-  // An empty configuration parameter is accepted and ignored.
-  hltPathNames_.clear();
-  if ( config.exists( hltPathConfig ) ) hltPathNames_ = config.getParameter< vector< string > >( hltPathConfig );
-  if ( hltPathNames_.empty() ) return errorReply_;
-  andOr_ = config.getParameter< bool >( andOrConfig );
-
-  /// Accessing the TriggerResults
+  // Accessing the TriggerResults
   event.getByLabel( hltInputTag_, hltTriggerResults_ );
   if ( ! hltTriggerResults_.isValid() ) {
     LogError( "triggerResultsValid" ) << "TriggerResults product with InputTag " << hltInputTag_.encode() << " not in event";
     return errorReply_;
   }
 
-  /// Getting the HLT configuartion from the provenance
+  // Getting the HLT configuration from the provenance
   bool changed( true );
   if ( ! hltConfig_.init( event, hltInputTag_.process(), changed ) ) {
     LogError( "hltConfigInit" ) << "HLT config initialization error with process name " << hltInputTag_.process();
@@ -61,7 +63,7 @@ bool SiStripTriggerHelper::accept( const Event & event, const ParameterSet & con
     return errorReply_;
   }
 
-  /// Determine acceptance of HLT path combination and return
+  // Determine acceptance of HLT path combination and return
   if ( andOr_ ) { // OR combination
     for ( vector< string >::const_iterator pathName = hltPathNames_.begin(); pathName != hltPathNames_.end(); ++pathName ) {
       if ( acceptPath( *pathName ) ) return true;
@@ -78,19 +80,26 @@ bool SiStripTriggerHelper::accept( const Event & event, const ParameterSet & con
 /// Was this event accepted by this particular HLT path?
 bool SiStripTriggerHelper::acceptPath( string hltPathName ) const
 {
+
+  // Check empty strings
   if ( hltPathName.empty() ) {
     LogError( "hltPathName" ) << "Empty path name";
     return errorReply_;
   }
+
+  // Negated paths
   bool notPath( false );
   if ( hltPathName.at( 0 ) == '~' ) {
     notPath = true;
     hltPathName.erase( 0, 1 );
+    // Check empty string again
     if ( hltPathName.empty() ) {
       LogError( "hltPathName" ) << "Empty (negated) path name";
       return errorReply_;
     }
   }
+
+  // Further error checks
   const unsigned indexPath( hltConfig_.triggerIndex( hltPathName ) );
   if ( indexPath == hltConfig_.size() ) {
     LogError( "hltPathInProcess" ) << "Path " << hltPathName << " is not found in process " << hltInputTag_.process();
@@ -100,6 +109,8 @@ bool SiStripTriggerHelper::acceptPath( string hltPathName ) const
     LogError( "hltPathError" ) << "Path " << hltPathName << " in error";
     return errorReply_;
   }
+
+  // Determine decision
   cout << "SiStripTriggerHelper: path "; // DEBUG
   if ( notPath ) cout << "~" << hltPathName; // DEBUG
   else           cout        << hltPathName; // DEBUG
