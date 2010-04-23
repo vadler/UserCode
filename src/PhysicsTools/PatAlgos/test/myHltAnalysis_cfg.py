b3b8bdfe3520091aec194f73216eeaee1290b097
vadler <>
2010-04-23 17:25:32 +0000

    Roll back after accidenatl commit.

--- src/PhysicsTools/PatAlgos/test/myHltAnalysis_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myHltAnalysis_cfg.py
@@ -8,8 +8,7 @@ process.GlobalTag.globaltag = cms.string('GR10_P_V2::All')
 # source
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
-#     '/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/14920B0A-0DE8-DE11-B138-002618943926.root'
-    'file:/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_5_6/output/patTuple_withRECO_6jets_Run132440.root'
+    '/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/14920B0A-0DE8-DE11-B138-002618943926.root'
   )
 )
 process.maxEvents = cms.untracked.PSet(
