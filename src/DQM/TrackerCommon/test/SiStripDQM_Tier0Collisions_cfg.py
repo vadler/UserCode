dd2c1899da57d5652fdb0764afa7a286dd670b3b
vadler <>
2010-05-11 16:59:53 +0000

    CMSSW_3_6_1.

--- src/DQM/TrackerCommon/test/SiStripDQM_Tier0Collisions_cfg.py
+++ src/DQM/TrackerCommon/test/SiStripDQM_Tier0Collisions_cfg.py
@@ -7,7 +7,7 @@ process.load( "FWCore.MessageService.MessageLogger_cfi" )
 
 process.load( "Configuration.StandardSequences.Services_cff" )
 process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
-process.GlobalTag.globaltag = 'GR_R_36X_V6::All'
+process.GlobalTag.globaltag = 'GR_R_36X_V9::All'
 process.load( "Configuration.StandardSequences.GeometryIdeal_cff" )
 process.load( "Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff" )
 
