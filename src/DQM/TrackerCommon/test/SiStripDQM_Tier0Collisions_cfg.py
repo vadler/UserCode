7adcaabab0b67ff3c622fb96f55d834a6b824e6b
vadler <>
2010-06-09 13:19:06 +0000

    Removing debug stuff.

--- src/DQM/TrackerCommon/test/SiStripDQM_Tier0Collisions_cfg.py
+++ src/DQM/TrackerCommon/test/SiStripDQM_Tier0Collisions_cfg.py
@@ -67,8 +67,7 @@ process.source = cms.Source( "PoolSource"
   )
 )
 process.maxEvents = cms.untracked.PSet(
-#   input = cms.untracked.int32( 100 )
-  input = cms.untracked.int32( 3 )
+  input = cms.untracked.int32( 100 )
 )
 
 # process.SiStripMonitorTrack.andOr          = False
