1b967b23a132521c28ff14a949871762ca81575b
vadler <>
2010-10-08 12:30:50 +0000

    CMSSW_3_9_0_pre6.

--- src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_cfg.py.old
+++ src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_cfg.py.old
@@ -1,6 +1,6 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 process.GlobalTag.globaltag = 'START38_V12::All'
-process.out.fileName        = '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_9_0_pre6/output/myPatTuple_addTriggerInfo.root'
+process.out.fileName        = '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_9_0_pre7/output/myPatTuple_addTriggerInfo.root'
 process.options.wantSummary = False
 process.maxEvents.input = 100
 
--- src/PhysicsTools/PatAlgos/test/myTest_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myTest_cfg.py
@@ -9,7 +9,7 @@ process.options = cms.untracked.PSet(
 
 process.source = cms.Source( "PoolSource"
 , fileNames = cms.untracked.vstring(
-    'file:/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_9_0_pre6/output/myPatTuple_addTriggerInfo.root'
+    'file:/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_9_0_pre7/output/myPatTuple_addTriggerInfo.root'
   )
 )
 process.maxEvents = cms.untracked.PSet(
@@ -17,7 +17,7 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.TFileService = cms.Service( "TFileService"
-, fileName = cms.string( '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_9_0_pre6/output/myTest.root' )
+, fileName = cms.string( '/afs/cern.ch/user/v/vadler/cms/PAT/CMSSW_3_9_0_pre7/output/myTest.root' )
 )
 
 process.triggerTest = cms.EDAnalyzer( "myTriggerTest"
