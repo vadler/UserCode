30feac20c24a9ec60c219b8b80d0794c934a567d
vadler <>
2010-12-08 15:39:20 +0000

    Polishing the new tool further.

--- src/PhysicsTools/PatAlgos/python/tools/myTools.py
+++ src/PhysicsTools/PatAlgos/python/tools/myTools.py
@@ -23,7 +23,7 @@ class PickRelValInputFile( ConfigToolBase ):
         self.addParameter( self._defaultParameters, 'relVal'      , 'RelValTTbar'                                                       , '' )
         self.addParameter( self._defaultParameters, 'dataTier'    , 'GEN-SIM-RECO'                                                      , '' )
         self.addParameter( self._defaultParameters, 'maxVersions' , 9                                                                   , '' )
-        self.addParameter( self._defaultParameters, 'maxFile'     , 1                                                                   , '' )
+        self.addParameter( self._defaultParameters, 'fileNumber'  , 1                                                                   , '' )
         self.addParameter( self._defaultParameters, 'debug'       , False                                                               , '' )
         self._parameters = copy.deepcopy( self._defaultParameters )
         self._comment = ""
@@ -35,7 +35,7 @@ class PickRelValInputFile( ConfigToolBase ):
                 , relVal       = None
                 , dataTier     = None
                 , maxVersions  = None
-                , maxFile      = None
+                , fileNumber   = None
                 , debug        = None
                 ):
         if condition is None:
@@ -50,8 +50,8 @@ class PickRelValInputFile( ConfigToolBase ):
             dataTier = self.getDefaultParameters()[ 'dataTier' ].value
         if maxVersions is None:
             maxVersions = self.getDefaultParameters()[ 'maxVersions' ].value
-        if maxFile is None:
-            maxFile = self.getDefaultParameters()[ 'maxFile' ].value
+        if fileNumber is None:
+            fileNumber = self.getDefaultParameters()[ 'fileNumber' ].value
         if debug is None:
             debug = self.getDefaultParameters()[ 'debug' ].value
         self.setParameter( 'condition'   , condition )
@@ -60,7 +60,7 @@ class PickRelValInputFile( ConfigToolBase ):
         self.setParameter( 'relVal'      , relVal )
         self.setParameter( 'dataTier'    , dataTier )
         self.setParameter( 'maxVersions' , maxVersions )
-        self.setParameter( 'maxFile'     , maxFile )
+        self.setParameter( 'fileNumber'  , fileNumber )
         self.setParameter( 'debug'       , debug )
         return self.apply()
 
@@ -71,50 +71,56 @@ class PickRelValInputFile( ConfigToolBase ):
         relVal       = self._parameters[ 'relVal'       ].value
         dataTier     = self._parameters[ 'dataTier'     ].value
         maxVersions  = self._parameters[ 'maxVersions'  ].value
-        maxFile      = self._parameters[ 'maxFile'      ].value
+        fileNumber   = self._parameters[ 'fileNumber'      ].value
         debug        = self._parameters[ 'debug'        ].value
-
         if debug:
             print 'DEBUG %s: Called with...'%( self._label )
             for key in self._parameters.keys():
                print '    %s:\t'%( key ),
-               print self._parameters[ key ].value
+               print self._parameters[ key ].value,
+               if self._parameters[ key ].value is self.getDefaultParameters()[ key ].value:
+                   print ' (default)'
+               else:
+                   print
 
         command      = 'nsls'
         rfdirPath    = '/store/relval/%s/%s/%s/%s-v'%( cmsswVersion, relVal, dataTier, globalTag )
         argument     = '/castor/cern.ch/cms%s'%( rfdirPath )
         filePath     = ''
         fileCount    = {}
-        lastValidVersion = 0
+        validVersion = 0
 
         for version in range( 1, maxVersions + 1 ):
             fileCount[ version ] = 0
             if debug:
-                print 'DEBUG %s: Checking directory %s%i'%( self._label, argument, version )
+                print 'DEBUG %s: Checking directory \'%s%i\''%( self._label, argument, version )
             directories = Popen( [ command, '%s%i'%( argument, version ) ], stdout = PIPE, stderr = PIPE ).communicate()[0]
             for directory in directories.splitlines():
                 files = Popen( [ command, '%s%i/%s'%( argument, version, directory ) ], stdout = PIPE, stderr = PIPE ).communicate()[0]
                 for file in files.splitlines():
                     if len( file ) > 0:
                         if debug:
-                            print 'DEBUG %s: File %s found'%( self._label, file )
+                            print 'DEBUG %s: File \'%s\' found'%( self._label, file )
                         fileCount[ version ] += 1
-                        lastValidVersion = version
-                    if fileCount[ version ] >= maxFile:
+                        validVersion = version
+                    if fileCount[ version ] >= fileNumber:
                         filePath = '%s%i/%s/%s'%( rfdirPath, version, directory, file )
                         break
                 if debug:
                     print 'DEBUG %s: %i file(s) found'%( self._label, fileCount[ version ] )
-                if fileCount[ version ] >= maxFile:
+                if fileCount[ version ] >= fileNumber:
                     break
-            if fileCount[ version ] >= maxFile:
+            if fileCount[ version ] >= fileNumber:
               break
-        if fileCount[ lastValidVersion ] < maxFile:
-            print 'pickRelValInputFile() Error'
-            print '    Only %i RelVal file(s) found in %s%i'%( fileCount[ lastValidVersion ], argument, lastValidVersion )
+        if validVersion == 0:
+            print 'ERROR pickRelValInputFile()'
+            print '    No RelVal file(s) found at all in \'%s*\''%( argument )
+        elif fileCount[ validVersion ] < fileNumber:
+            print 'ERROR pickRelValInputFile()'
+            print '    Only %i RelVal file(s) found in \'%s%i\''%( fileCount[ validVersion ], argument, validVersion )
 
         if debug:
-            print 'DEBUG %s: returning file %s'%( self._label, filePath )
+            print 'DEBUG %s: returning file \'%s\''%( self._label, filePath )
         return filePath
 
 pickRelValInputFile = PickRelValInputFile()
