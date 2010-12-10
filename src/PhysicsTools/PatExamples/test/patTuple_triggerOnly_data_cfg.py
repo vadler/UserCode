22b4b9cea50f2fe344095b041deb4788a87c2773
vadler <>
2010-12-10 13:40:01 +0000

    Further updates.

--- src/PhysicsTools/PatExamples/test/patTuple_triggerOnly_data_cfg.py
+++ src/PhysicsTools/PatExamples/test/patTuple_triggerOnly_data_cfg.py
@@ -4,9 +4,8 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 ## ... and modify it according to the needs:
 # use latest ReReco as input
-process.source.fileNames = [
-  '/store/data/Run2010A/Mu/AOD/Nov4ReReco_v1/0011/D2E5D86F-AEEC-DF11-B261-0017A4771028.root' # /Mu/Run2010A-Nov4ReReco_v1/AOD, run 144112, 17717 events
-]
+from PhysicsTools.PatExamples.samplesCERN_cff import dataMu
+process.source.fileNames = dataMu
 # use the correct conditions
 process.GlobalTag.globaltag = 'GR_R_38X_V15::All'
 # use a sufficient number of events
@@ -15,10 +14,10 @@ process.maxEvents.input = 1000
 process.out.fileName = 'patTuple_triggerOnly_dataReReco.root'
 
 ## ---
-## Define the path
+## Define the path as empty skeleton
 ## ---
 process.p = cms.Path(
-) # empty skeleton
+)
 
 ### ===========
 ### PAT trigger
@@ -35,16 +34,16 @@ switchOnTrigger( process, sequence = 'p' ) # overwrite sequence default "patDefa
 ## --
 # add L1 algorithms' collection
 process.patTrigger.addL1Algos     = cms.bool( True ) # default: 'False'
-# add L1 objects collection-wise
-process.patTrigger.l1ExtraMu      = cms.InputTag( 'l1extraParticles', ''           , 'RECO' )
-process.patTrigger.l1ExtraNoIsoEG = cms.InputTag( 'l1extraParticles', 'NonIsolated', 'RECO' )
-process.patTrigger.l1ExtraIsoEG   = cms.InputTag( 'l1extraParticles', 'Isolated'   , 'RECO' )
-process.patTrigger.l1ExtraCenJet  = cms.InputTag( 'l1extraParticles', 'Central'    , 'RECO' )
-process.patTrigger.l1ExtraForJet  = cms.InputTag( 'l1extraParticles', 'Forward'    , 'RECO' )
-process.patTrigger.l1ExtraTauJet  = cms.InputTag( 'l1extraParticles', 'Tau'        , 'RECO' )
-process.patTrigger.l1ExtraETM     = cms.InputTag( 'l1extraParticles', 'MET'        , 'RECO' )
-process.patTrigger.l1ExtraHTM     = cms.InputTag( 'l1extraParticles', 'MHT'        , 'RECO' )
-# save references to original L1 objects (no tool yet)
+# add L1 objects collection-wise (latest collection)
+process.patTrigger.l1ExtraMu      = cms.InputTag( 'l1extraParticles', ''            )
+process.patTrigger.l1ExtraNoIsoEG = cms.InputTag( 'l1extraParticles', 'NonIsolated' )
+process.patTrigger.l1ExtraIsoEG   = cms.InputTag( 'l1extraParticles', 'Isolated'    )
+process.patTrigger.l1ExtraCenJet  = cms.InputTag( 'l1extraParticles', 'Central'     )
+process.patTrigger.l1ExtraForJet  = cms.InputTag( 'l1extraParticles', 'Forward'     )
+process.patTrigger.l1ExtraTauJet  = cms.InputTag( 'l1extraParticles', 'Tau'         )
+process.patTrigger.l1ExtraETM     = cms.InputTag( 'l1extraParticles', 'MET'         )
+process.patTrigger.l1ExtraHTM     = cms.InputTag( 'l1extraParticles', 'MHT'         )
+# save references to original L1 objects
 process.patTrigger.saveL1Refs = cms.bool( True ) # default: 'False'
 # update event content to save
 switchOnTrigger( process, sequence = 'p' )       # called once more to update the event content according to the changed parameters!!!
