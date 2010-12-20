2b63b3046414afa1c8774bd856da998cb9a2cbe7
vadler <>
2010-12-20 11:53:26 +0000

    Improved Doxygen.

--- src/PhysicsTools/PatUtils/interface/TriggerHelper.h
+++ src/PhysicsTools/PatUtils/interface/TriggerHelper.h
@@ -37,23 +37,41 @@ namespace pat {
 
       public:
 
-        /// constructors and destructor
+        /// Constructors and Destructor
+
+        /// Default constructor
         TriggerMatchHelper() {};
+
+        /// Destructor
         ~TriggerMatchHelper() {};
 
-        /// functions
-        TriggerObjectRef                          triggerMatchObject( const reco::CandidateBaseRef & candRef, const TriggerObjectMatch * matchResult, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
-        TriggerObjectRef                          triggerMatchObject( const reco::CandidateBaseRef & candRef, const std::string & labelMatcher, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
-        template< class C > TriggerObjectRef      triggerMatchObject( const edm::Handle< C > & candCollHandle, const size_t iCand, const TriggerObjectMatch * matchResult, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
-        template< class C > TriggerObjectRef      triggerMatchObject( const edm::Handle< C > & candCollHandle, const size_t iCand, const std::string & labelMatcher, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
-        TriggerObjectMatchMap                     triggerMatchObjects( const reco::CandidateBaseRef & candRef, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// Methods
+
+        /// Get a reference to the trigger objects matched to a certain physics object given by a reference for a certain matcher module
+        /// ... by resulting association
+        TriggerObjectRef triggerMatchObject( const reco::CandidateBaseRef & candRef, const TriggerObjectMatch * matchResult, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// ... by matcher module label
+        TriggerObjectRef triggerMatchObject( const reco::CandidateBaseRef & candRef, const std::string & labelMatcher      , const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// Get a reference to the trigger objects matched to a certain physics object given by a collection and index for a certain matcher module
+        /// ... by resulting association
+        template< class C > TriggerObjectRef triggerMatchObject( const edm::Handle< C > & candCollHandle, const size_t iCand, const TriggerObjectMatch * matchResult, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// ... by matcher module label
+        template< class C > TriggerObjectRef triggerMatchObject( const edm::Handle< C > & candCollHandle, const size_t iCand, const std::string & labelMatcher      , const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// Get a table of references to all trigger objects matched to a certain physics object given by a reference
+        TriggerObjectMatchMap triggerMatchObjects( const reco::CandidateBaseRef & candRef, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// Get a table of references to all trigger objects matched to a certain physics object given by a collection and index
         template< class C > TriggerObjectMatchMap triggerMatchObjects( const edm::Handle< C > & candCollHandle, const size_t iCand, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
-        reco::CandidateBaseRefVector              triggerMatchCandidates( const pat::TriggerObjectRef & objectRef, const TriggerObjectMatch * matchResult, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
-        reco::CandidateBaseRefVector              triggerMatchCandidates( const pat::TriggerObjectRef & objectRef, const std::string & labelMatcher, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// Get a vector of references to the phyics objects matched to a certain trigger object given by a reference for a certain matcher module
+        /// ... by resulting association
+        reco::CandidateBaseRefVector triggerMatchCandidates( const pat::TriggerObjectRef & objectRef, const TriggerObjectMatch * matchResult, const edm::Event & event, const TriggerEvent & triggerEvent ) const;
+        /// ... by matcher module label
+        reco::CandidateBaseRefVector triggerMatchCandidates( const pat::TriggerObjectRef & objectRef, const std::string & labelMatcher      , const edm::Event & event, const TriggerEvent & triggerEvent ) const;
 
     };
 
-    /// function templates
+    // Method Templates
+
+    // Get a reference to the trigger objects matched to a certain physics object given by a collection and index for a certain matcher module
     template< class C > TriggerObjectRef TriggerMatchHelper::triggerMatchObject( const edm::Handle< C > & candCollHandle, const size_t iCand, const TriggerObjectMatch * matchResult, const edm::Event & event, const TriggerEvent & triggerEvent ) const
     {
       const reco::CandidateBaseRef candRef( edm::Ref< C >( candCollHandle, iCand ) );
@@ -63,6 +81,8 @@ namespace pat {
     {
       return triggerMatchObject( candCollHandle, iCand, triggerEvent.triggerObjectMatchResult( labelMatcher ), event, triggerEvent );
     }
+
+    // Get a table of references to all trigger objects matched to a certain physics object given by a collection and index
     template< class C > TriggerObjectMatchMap TriggerMatchHelper::triggerMatchObjects( const edm::Handle< C > & candCollHandle, const size_t iCand, const edm::Event & event, const TriggerEvent & triggerEvent ) const
     {
       const reco::CandidateBaseRef candRef( edm::Ref< C >( candCollHandle, iCand ) );
