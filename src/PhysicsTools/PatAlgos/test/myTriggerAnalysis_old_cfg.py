ad0e049ee17e97ea74d375f832c746a11277ebab
vadler <>
2011-02-21 19:52:33 +0000

    Trying to incorporate 'L1GtAnalyzer' via 'MessageLogger' (fail).

--- src/PhysicsTools/PatAlgos/test/myTriggerAnalysis_old_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myTriggerAnalysis_old_cfg.py
@@ -35,9 +35,22 @@ process.load( "HLTrigger.HLTcore.triggerSummaryAnalyzerAOD_cfi" )
 process.load( "L1Trigger.GlobalTriggerAnalyzer.l1GtAnalyzer_cfi" )
 process.l1GtAnalyzer.AlgorithmName = "L1_SingleMu7"
 process.l1GtAnalyzer.ConditionName = "SingleMu_0x0B"
+process.load( "L1Trigger.GlobalTriggerAnalyzer.l1GtTrigReport_cfi" )
+process.l1GtTrigReport.PrintVerbosity = 101
 
 process.p = cms.Path(
   process.l1GtAnalyzer
++ process.l1GtTrigReport
 + process.hltEventAnalyzerAOD
 + process.triggerSummaryAnalyzerAOD
 )
+
+# Message logger
+process.load( "FWCore.MessageService.MessageLogger_cfi" )
+process.MessageLogger.debugModules.append( 'l1GtAnalyzer' ) # FIXME: this does not work yet
+process.MessageLogger.categories.append( 'L1GtAnalyzer' )
+process.MessageLogger.categories.append( 'L1GtTrigReport' )
+process.MessageLogger.cerr.threshold = 'DEBUG'
+process.MessageLogger.cerr.DEBUG          = cms.untracked.PSet( limit = cms.untracked.int32( 0 ) )
+process.MessageLogger.cerr.L1GtAnalyzer   = cms.untracked.PSet( limit = cms.untracked.int32( -1 ) )
+process.MessageLogger.cerr.L1GtTrigReport = cms.untracked.PSet( limit = cms.untracked.int32( -1 ) )
