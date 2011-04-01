03890a7d4dd7dc1d87210b38828113e8e4aba9d5
vadler <>
2011-04-01 11:09:52 +0000

    Remove obsolete tool completely.

--- src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_mcReDigi_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_mcReDigi_cfg.py
@@ -3,10 +3,6 @@ cmsswBase = os.getenv( 'CMSSW_BASE' )
 
 from PhysicsTools.PatAlgos.patTemplate_cfg import * # conditions contain L1 menu 'L1GtTriggerMenu_L1Menu_Collisions2011_v0a_mc'
 
-## To run on 3XY MC
-#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run41xOn3yzMcInput
-#run42xOn3yzMcInput( process )
-
 process.options.wantSummary = False
 process.source.fileNames    = [ 'rfio:/castor/cern.ch/user/v/vadler/cms/PAT/data/TTJets_TuneD6T_7TeV-madgraph-tauola__Spring11-PU_S1_START311_V1G1-v1__AODSIM/D42E9BB3-C759-E011-B8DC-00A0D1EE8E74.root' ]
 process.maxEvents.input     = 100
