d8eab3b32b964b9d12604d12e3f7fd4446b4a863
vadler <>
2011-07-04 12:30:29 +0000

    Roll-back.

--- src/PhysicsTools/PatExamples/test/checkHlt_cfg.py
+++ src/PhysicsTools/PatExamples/test/checkHlt_cfg.py
@@ -1,15 +1,12 @@
 # Start with a skeleton process which gets imported with the following line
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
-process.source.fileNames  = pickRelValInputFiles( relVal    = 'RelValZMM'
-                                                , globalTag = 'START42_V12'
-                                                )
 process.maxEvents.input = 1
 
 # HLT analyzers
 process.load( "HLTrigger.HLTcore.hltEventAnalyzerAOD_cfi" )
 process.hltEventAnalyzerAOD.triggerName = cms.string( '@' )
 process.load( "HLTrigger.HLTcore.triggerSummaryAnalyzerAOD_cfi" )
-
+                                                                       
 process.p = cms.Path(
     process.hltEventAnalyzerAOD       +
     process.triggerSummaryAnalyzerAOD
