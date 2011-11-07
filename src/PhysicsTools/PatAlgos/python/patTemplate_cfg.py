f298872a4c76ebc50f49357ab72ac3504bde2d61
vadler <>
2011-11-07 11:57:06 +0000

    Update input RelVals due to removal of old ones.

--- src/PhysicsTools/PatAlgos/python/patTemplate_cfg.py
+++ src/PhysicsTools/PatAlgos/python/patTemplate_cfg.py
@@ -12,9 +12,9 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_4_2_4'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_4_2_9_HLT1'
                         , relVal        = 'RelValTTbar'
-                        , globalTag     = 'START42_V12'
+                        , globalTag     = 'START42_V14A'
                         , numberOfFiles = 1
                         )
     )
