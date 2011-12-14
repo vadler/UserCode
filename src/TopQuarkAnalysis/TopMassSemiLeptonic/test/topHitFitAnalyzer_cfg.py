64252e7211bdd8c95f0c4730f61dd4084f268871
vadler <>
2011-12-14 13:39:33 +0000

    This commit was manufactured by cvs2svn to create branch 'B4_2_X'.
    Cherrypick from master 2011-10-14 15:09:38 UTC vadler 'Get the inversion of p resp. p_t right and add electrons.':
        src/TopQuarkAnalysis/Configuration/python/patRefSel_basics_cff.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_common_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cff.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_inputModule_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_outputModule_cff.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_outputModule_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_refMuJets_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_triggerMatching_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_triggerSelection_cff.py
        src/TopQuarkAnalysis/Configuration/test/README
        src/TopQuarkAnalysis/Configuration/test/gentPatSkimPF2PAT_cfg.py
        src/TopQuarkAnalysis/Configuration/test/gentPatSkim_cfg.py
        src/TopQuarkAnalysis/TopHitFit/tags.txt
        src/TopQuarkAnalysis/TopHitFit/test/hitFitPatSkimPF2PAT_cfg.py
        src/TopQuarkAnalysis/TopHitFit/test/ttSemiLepEvtBuilder_cfg.py
        src/TopQuarkAnalysis/TopMassSemiLeptonic/BuildFile.xml
        src/TopQuarkAnalysis/TopMassSemiLeptonic/plugins/BuildFile.xml
        src/TopQuarkAnalysis/TopMassSemiLeptonic/plugins/TopHitFitAnalyzer.cc
        src/TopQuarkAnalysis/TopMassSemiLeptonic/python/topHitFitAnalyzer_cfi.py
        src/TopQuarkAnalysis/TopMassSemiLeptonic/tags.txt
        src/TopQuarkAnalysis/TopMassSemiLeptonic/test/topHitFitAnalyzer_cfg.py
    Cherrypick from master 2011-12-14 13:39:31 UTC vadler 'Commit.':
        src/TopQuarkAnalysis/Configuration/python/patRefSel_PF2PAT.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_goodVertex_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_refMuJets.py
        src/TopQuarkAnalysis/Configuration/tags.txt
        src/TopQuarkAnalysis/Configuration/test/patRefSel_allJets_cfg.py
        src/TopQuarkAnalysis/Configuration/test/patRefSel_muJets_cfg.py

---
+++ src/TopQuarkAnalysis/TopMassSemiLeptonic/test/topHitFitAnalyzer_cfg.py
@@ -0,0 +1,125 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+### Steering
+
+# Misc
+runTest = True
+
+# Input
+runOnMC = True
+sample  = 'RelValTTbar'
+#sample  = 'RelValZMM'
+#sample  = 'RelValWM'
+#sample  = 'Mu'
+#sample  = 'Jet'
+
+# Event selection
+hitFitProcess = 'HitFit'
+
+
+### Initialization
+
+process = cms.Process( "TEST" )
+
+
+### Logging
+
+reportEvery = 1000
+if runTest:
+  reportEvery = 1
+process.load( "FWCore.MessageService.MessageLogger_cfi" )
+process.MessageLogger.cerr.FwkReport.reportEvery = reportEvery
+process.options = cms.untracked.PSet(
+  wantSummary = cms.untracked.bool( True )
+)
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.categories.append('TtSemiLeptonicEvent')
+process.MessageLogger.cerr.TtSemiLeptonicEvent = cms.untracked.PSet(
+  limit = cms.untracked.int32( -1 )
+)
+
+
+### Input
+
+inputFiles = [ 'rfio:%s/cms/Top/data/hitFitPatSkimPF2PAT_%s.root'%( os.getenv( "CASTOR_HOME" ), sample )
+             ]
+if runTest:
+  inputFiles = [ 'file:hitFitPatSkimPF2PAT.root'
+               ]
+process.source = cms.Source(
+  "PoolSource"
+, fileNames = cms.untracked.vstring( inputFiles )
+)
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32( -1 )
+)
+
+
+### Output
+
+outputFile = '%s/output/topHitFitAnalyzer_%s.root'%( os.getenv( "CMSSW_BASE" ), sample )
+if runTest:
+  outputFile = 'topHitFitAnalyzer.root'
+process.TFileService = cms.Service(
+  "TFileService"
+, fileName = cms.string( outputFile )
+)
+
+
+### Event selection
+
+# Trigger
+process.load( "HLTrigger.HLTfilters.hltHighLevel_cfi" )
+process.hltHighLevel.TriggerResultsTag = cms.InputTag( 'TriggerResults::%s'%( hitFitProcess ) )
+process.hltHighLevel.HLTPaths          = [ 'HitFit_PF2PATHitFit'
+                                         ]
+process.hltHighLevel_Reference = process.hltHighLevel.clone( HLTPaths = [ 'HitFit_ReferenceHitFit'
+                                                                        ]
+                                                           )
+
+
+### Analyzer
+
+process.load( "TopQuarkAnalysis.TopMassSemiLeptonic.topHitFitAnalyzer_cfi" )
+process.topHitFitAnalyzer.verbosity = 1
+if runTest:
+  process.topHitFitAnalyzer.verbosity = 11
+process.topHitFitAnalyzer_Reference = process.topHitFitAnalyzer.clone( ttSemiLeptonicEvent = 'ttSemiLepEventReference'
+                                                                     , patMuons            = 'referencePatMuons'
+                                                                     , patJets             = 'referencePatJets'
+                                                                     )
+process.topHitFitAnalyzer_ReferenceBTag = process.topHitFitAnalyzer.clone( ttSemiLeptonicEvent = 'ttSemiLepEventReferenceBTag'
+                                                                         , patMuons            = 'referencePatMuons'
+                                                                         , patJets             = 'referencePatJets'
+                                                                         )
+
+
+### Paths
+
+process.p = cms.Path(
+  process.hltHighLevel
+* process.topHitFitAnalyzer
+)
+
+process.p_Reference = cms.Path(
+  process.hltHighLevel_Reference
+* process.topHitFitAnalyzer_Reference
+)
+
+process.p_ReferenceBTag = cms.Path(
+  process.hltHighLevel_Reference
+* process.topHitFitAnalyzer_ReferenceBTag
+)
+
+
+### Messages
+
+print
+print 'Log file:'
+if runTest:
+  print 'topHitFitAnalyzer.log'
+else:
+  print '%s/output/topHitFitAnalyzer_%s.log'%( os.getenv( "CMSSW_BASE" ), sample )
+print '================================================================================'
+print
