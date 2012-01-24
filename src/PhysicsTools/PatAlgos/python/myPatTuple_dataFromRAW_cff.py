8b706e3092625ed468a23d1295463643dfb4f9d2
vadler <>
2012-01-24 10:43:30 +0000

    Proper LFN notation.

--- src/PhysicsTools/PatAlgos/python/myPatTuple_dataFromRAW_cff.py
+++ src/PhysicsTools/PatAlgos/python/myPatTuple_dataFromRAW_cff.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 fileNames = cms.PSet(
   fileNames = cms.untracked.vstring(
-    'rfio:/castor/cern.ch/cms/store/data/Run2011A/MuHad/RAW/v1/000/165/121/30B69E98-C67F-E011-8697-003048D37560.root'
-  , 'rfio:/castor/cern.ch/cms/store/data/Run2011A/MuHad/RAW/v1/000/165/121/80E66677-C27F-E011-8A15-001D09F29321.root'
-  , 'rfio:/castor/cern.ch/cms/store/data/Run2011A/MuHad/RAW/v1/000/165/121/8E69B419-B57F-E011-821E-001617C3B65A.root'
-  , 'rfio:/castor/cern.ch/cms/store/data/Run2011A/MuHad/RAW/v1/000/165/121/A219173E-B07F-E011-ACC8-0030487CD6DA.root'
-  , 'rfio:/castor/cern.ch/cms/store/data/Run2011A/MuHad/RAW/v1/000/165/121/B028587F-D47F-E011-94FD-0030487CD906.root'
-  , 'rfio:/castor/cern.ch/cms/store/data/Run2011A/MuHad/RAW/v1/000/165/121/CCCA933C-BE7F-E011-A992-0030486730C6.root'
-  , 'rfio:/castor/cern.ch/cms/store/data/Run2011A/MuHad/RAW/v1/000/165/121/DCC9E8A9-BA7F-E011-A73A-003048D37580.root'
+    '/store/data/Run2011A/MuHad/RAW/v1/000/165/121/30B69E98-C67F-E011-8697-003048D37560.root'
+  , '/store/data/Run2011A/MuHad/RAW/v1/000/165/121/80E66677-C27F-E011-8A15-001D09F29321.root'
+  , '/store/data/Run2011A/MuHad/RAW/v1/000/165/121/8E69B419-B57F-E011-821E-001617C3B65A.root'
+  , '/store/data/Run2011A/MuHad/RAW/v1/000/165/121/A219173E-B07F-E011-ACC8-0030487CD6DA.root'
+  , '/store/data/Run2011A/MuHad/RAW/v1/000/165/121/B028587F-D47F-E011-94FD-0030487CD906.root'
+  , '/store/data/Run2011A/MuHad/RAW/v1/000/165/121/CCCA933C-BE7F-E011-A992-0030486730C6.root'
+  , '/store/data/Run2011A/MuHad/RAW/v1/000/165/121/DCC9E8A9-BA7F-E011-A73A-003048D37580.root'
   )
 )
