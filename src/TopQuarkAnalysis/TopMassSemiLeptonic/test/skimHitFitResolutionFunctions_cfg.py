c42fe3a11d031cff19877a74cae53459b5dfebf1
vadler <>
2012-01-25 12:04:25 +0000

    CMSSW_4_4_3.

--- src/TopQuarkAnalysis/TopMassSemiLeptonic/test/skimHitFitResolutionFunctions_cfg.py
+++ src/TopQuarkAnalysis/TopMassSemiLeptonic/test/skimHitFitResolutionFunctions_cfg.py
@@ -143,7 +143,8 @@ process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff"
 # process.GlobalTag.globaltag = autoCond[ 'startup' ]
 process.GlobalTag.globaltag = 'START44_V12::All' # JEC2011V12 as in https://hypernews.cern.ch/HyperNews/CMS/get/jes/303.html
 if runCrab:
-  process.GlobalTag.globaltag = 'START44_V5D::All' # Fall11_R3
+#   process.GlobalTag.globaltag = 'START44_V5D::All' # Fall11_R3
+  process.GlobalTag.globaltag = 'START44_V9C::All' # Fall11_R4
 
 
 ### Input
