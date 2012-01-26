0a79f3971078146b2dec39a6ad2c0413e4e58b47
vadler <>
2012-01-26 17:53:23 +0000

    Have old and new version of Haryo's resolutions available.

--- src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/produceExistingTopHitFitResolutionFunctions_cfg.py
+++ src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/produceExistingTopHitFitResolutionFunctions_cfg.py
@@ -2,6 +2,9 @@ import os
 
 import FWCore.ParameterSet.Config as cms
 
+# era = 'Spring10'
+era = 'Summer11'
+
 process = cms.PSet()
 
 process.existing = cms.PSet(
@@ -17,13 +20,13 @@ process.existing = cms.PSet(
                               , 500.
                               , 300.
                               )
-, resolutionFiles = cms.vstring( 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafMuonResolution.txt'
-                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafElectronResolution.txt'
-                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafUdscJetResolution.txt'
-                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafBJetResolution.txt'
-                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafKtResolution.txt'
+, resolutionFiles = cms.vstring( 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafMuonResolution_%s.txt'%( era )
+                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafElectronResolution_%s.txt'%( era )
+                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafUdscJetResolution_%s.txt'%( era )
+                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafBJetResolution_%s.txt'%( era )
+                               , 'TopQuarkAnalysis/TopHitFit/data/resolution/tqafKtResolution_%s.txt'%( era )
                                )
 , resolutionFunction        = cms.string( 'sqrt(([0]*[0]*x+[1]*[1])*x+[2]*[2])' )
 , resolutionFunctionInverse = cms.string( 'sqrt(([0]*[0]/x+[1]*[1])/x+[2]*[2])' )
-, outputFile = cms.string( 'file:%s/output/existingHitFitResolutionFunctions.root'%( os.getenv( "CMSSW_BASE" ) ) )
+, outputFile = cms.string( 'file:%s/output/existingHitFitResolutionFunctions_%s.root'%( os.getenv( "CMSSW_BASE" ), era ) )
 )
