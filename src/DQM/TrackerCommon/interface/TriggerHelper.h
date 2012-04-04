06c2077354b77a42c1ba7720fc5596f5c98d750f
vadler <>
2012-04-04 19:07:55 +0000

    This commit was manufactured by cvs2svn to create tag 'V01-01-00'.
    Sprout from PAT-B1_6_X 2008-04-14 22:33:36 UTC vadler 'Added trigger matching to standard layer0/layer1 workflows.'
    Cherrypick from master 2012-01-30 23:18:13 UTC vadler 'Simplifdied trigger selections.':
        src/TopQuarkAnalysis/Configuration/python/patRefSel_PF2PAT.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_basics_cff.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_common_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_goodVertex_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_inputModule_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_outputModule_cff.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_outputModule_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_refMuJets.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_refMuJets_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_triggerMatching_cfi.py
        src/TopQuarkAnalysis/Configuration/python/patRefSel_triggerSelection_cff.py
    Cherrypick from master 2010-02-04 22:16:21 UTC vadler 'Enabled logical expression parsing for HLT filter.':
        src/DQM/TrackerCommon/BuildFile
        src/DQM/TrackerCommon/interface/TriggerHelper.h
        src/DQM/TrackerCommon/python/TriggerHelper_cff.py
        src/DQM/TrackerCommon/src/TriggerHelper.cc
        src/DQM/TrackerCommon/tags.txt
        src/DQM/TrackerCommon/test/SiStripDQM_Tier0Collisions_cfg.py
        src/DQM/TrackerCommon/test/checkHlt_cfg.py
    Cherrypick from B4_4_X 2012-04-04 19:07:53 UTC vadler 'Commit.':
        src/TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cff.py
        src/TopQuarkAnalysis/Configuration/tags.txt
        src/TopQuarkAnalysis/Configuration/test/README
        src/TopQuarkAnalysis/Configuration/test/gentPatSkimPF2PATBroc_cfg.py
        src/TopQuarkAnalysis/Configuration/test/gentPatSkimPF2PAT_cfg.py
        src/TopQuarkAnalysis/Configuration/test/gentPatSkim_cfg.py
        src/TopQuarkAnalysis/Configuration/test/patRefSel_allJets_cfg.py
        src/TopQuarkAnalysis/Configuration/test/patRefSel_muJets_cfg.py
    Delete:
        src/DataFormats/PatCandidates/BuildFile
        src/DataFormats/PatCandidates/interface/PATObject.h
        src/DataFormats/PatCandidates/interface/TriggerPrimitive.h
        src/DataFormats/PatCandidates/src/TriggerPrimitive.cc
        src/DataFormats/PatCandidates/src/classes.h
        src/DataFormats/PatCandidates/src/classes_def.xml

---
+++ src/DQM/TrackerCommon/interface/TriggerHelper.h
@@ -0,0 +1,82 @@
+#ifndef TriggerHelper_H
+#define TriggerHelper_H
+
+
+// -*- C++ -*-
+//
+// Package:    DQM/TrackerCommon
+// Class:      TriggerHelper
+//
+// $Id: TriggerHelper.h,v 1.2 2010/02/02 22:31:11 vadler Exp $
+//
+/**
+  \class    TriggerHelper TriggerHelper.h "DQM/TrackerCommon/interface/TriggerHelper.h"
+  \brief    Provides a code based selection for HLT path combinations in order to have no failing filters in the CMSSW path.
+
+   [...]
+
+  \author   Volker Adler
+  \version  $Id: TriggerHelper.h,v 1.2 2010/02/02 22:31:11 vadler Exp $
+*/
+
+
+#include <string>
+#include <vector>
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "L1Trigger/GlobalTriggerAnalyzer/interface/L1GtUtils.h"
+#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/Scalers/interface/DcsStatus.h"
+
+class TriggerHelper {
+
+    // Data members
+    // L1 access
+    L1GtUtils l1Gt_;
+    // L1 filter configuration parameters
+    bool errorReplyL1_;
+    // HLT configuration
+    HLTConfigProvider hltConfig_;
+    // HLT filter configuration parameters
+    edm::InputTag hltInputTag_;
+    edm::Handle< edm::TriggerResults > hltTriggerResults_;
+    bool errorReplyHlt_;
+    // DCS filter configuration parameters
+    edm::Handle< DcsStatusCollection > dcsStatus_;
+    bool errorReplyDcs_;
+
+  public:
+
+    // Constructors and destructor
+    TriggerHelper();
+    ~TriggerHelper() {};
+
+    // Public methods
+    bool accept( const edm::Event & event, const edm::EventSetup & setup, const edm::ParameterSet & config ); // L1, HLT and DCS filters combined
+    bool accept( const edm::Event & event, const edm::ParameterSet & config );                                // HLT and DCS filters only
+
+  private:
+
+    // Private methods
+
+    // L1
+    bool acceptL1( const edm::Event & event, const edm::EventSetup & setup, const edm::ParameterSet & config );
+    bool acceptL1LogicalExpression( const edm::Event & event, std::string l1LogicalExpression );
+
+    // HLT
+    bool acceptHlt( const edm::Event & event, const edm::ParameterSet & config );
+    bool acceptHltLogicalExpression( std::string hltLogicalExpression ) const;
+
+    // DCS
+    bool acceptDcs( const edm::Event & event, const edm::ParameterSet & config );
+    bool acceptDcsPartition( int dcsPartition ) const;
+
+    // Algos
+    bool negate( std::string & word ) const;
+
+};
+
+
+#endif
