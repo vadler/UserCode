54f664965f423fc11bd943c035567da3bc3716f5
vadler <>
2012-05-04 18:34:02 +0000

    CVS test.

--- src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfoOnly_dataOld_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfoOnly_dataOld_cfg.py
@@ -3,7 +3,7 @@ cmsswBase = os.getenv( "CMSSW_BASE" )
 
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process( "PAT" )
+process = cms.Process( "PT" )
 
 ## Options and Output Report
 process.options = cms.untracked.PSet(
