7ad2840ca2e78dcbcde860c738fa51d379a6d7ca
vadler <>
2012-08-06 16:33:25 +0000

    Ok. Starting to work fine now :-)

--- src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/plotTopHitFitResolutionFunctions.C
+++ src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/plotTopHitFitResolutionFunctions.C
@@ -62,16 +62,108 @@ int main(  int argc, char * argv[] )
   // Get the configurations
   const edm::ParameterSet & process_( edm::readPSetsFrom( argv[ 1 ] )->getParameter< edm::ParameterSet >( "process" ) );
   const bool verbose_( process_.getParameter< bool >( "verbose" ) );
-  // Configuration for fitting new resolution functions
-  const edm::ParameterSet & plotter_( process_.getParameter< edm::ParameterSet >( "plotter" ) );
-  const std::string outFile_( plotter_.getParameter< std::string >( "outputFile" ) );
-  const std::vector< std::string > inFiles_( plotter_.getParameter< std::vector< std::string > >( "inputFiles" ) );
-  const std::vector< std::string > objCats_( plotter_.getParameter< std::vector< std::string > >( "objectCategories" ) );                                          // input file with RECO-GEN distributions
-  const std::string evtSel_( plotter_.getParameter< std::string >( "selection" ) );
+  const std::vector< std::string > objCats_( process_.getParameter< std::vector< std::string > >( "objectCategories" ) );   // object categories
+  const bool overwrite_(  process_.getParameter< bool >( "overwrite" ));
+  const bool usePileUp_( process_.getParameter< bool >( "usePileUp" ) );
+  const bool useAlt_( process_.getParameter< bool >( "useAlt" ) );
+  const bool useSymm_( process_.getParameter< bool >( "useSymm" ) );
+  const bool refGen_( process_.getParameter< bool >( "refGen" ) );
+  const bool refSel_( process_.getParameter< bool >( "refSel" ) );
+  // Configuration for in- & output
+  const edm::ParameterSet & io_( process_.getParameter< edm::ParameterSet >( "io" ) );
+  const std::vector< std::string > inFiles_( io_.getParameter< std::vector< std::string > >( "inputFiles" ) );
+  const std::string sample_( io_.getParameter< std::string >( "sample" ) );
+  const std::string outFile_( io_.getParameter< std::string >( "outputFile" ) );
+  const std::string resolutionFile_( io_.getParameter< std::string >( "resolutionFile" ) );
+  // Configuration for plotting resolution functions
+  const edm::ParameterSet & plot_( process_.getParameter< edm::ParameterSet >( "plot" ) );
+  const bool onlyExisting_( plot_.getParameter< bool >( "onlyExisting" ) );
+  const bool writeFiles_( plot_.getParameter< bool >( "writeFiles" ) && onlyExisting_ );
+
+  std::vector< std::vector< bool > > nominalInv_( objCats_.size() );
 
   // Set constants
-  const std::string dirClassName_( "TDirectoryFile" );
-  const std::string funcClassName_( "TF1" );
+  std::string evtSel_( "analyzeHitFit" );
+  if ( refSel_ ) evtSel_.append( "Reference" );
+  const std::string nameDirClass( "TDirectoryFile" );
+  const std::string nameFuncClass( "TF1" );
+
+
+  // Use existing resolution functions
+
+  std::cout << std::endl
+            << argv[ 0 ] << " --> INFO:" << std::endl
+            << "    accessing existing resolution functions from resolution file '" << resolutionFile_ << "'" << std::endl;
+
+  // Open resolution file
+  TFile * resolutionFile( TFile::Open( resolutionFile_.c_str(), "READ" ) );
+  if ( ! resolutionFile ) {
+    std::cout << argv[ 0 ] << " --> ERROR:" << std::endl
+              << "   resolution file '" << resolutionFile_ << "' not found" << std::endl;
+    returnStatus_ += 0x100;
+    return returnStatus_;
+  }
+
+  // Loop over objects and quantities
+
+  for ( unsigned uCat = 0; uCat < objCats_.size(); ++uCat ) {
+    const std::string objCat( objCats_.at( uCat ) );
+    TDirectory * dirCatRes_( dynamic_cast< TDirectory* >( resolutionFile->Get( objCat.c_str() ) ) );
+    if ( ! dirCatRes_ ) {
+      std::cout << argv[ 0 ] << " --> WARNING:" << std::endl
+                << "    object category '" << objCat << "' does not exist in resolution file" << std::endl;
+      continue;
+    }
+
+    TList * listCatRes( dirCatRes_->GetListOfKeys() );
+    TIter nextInListCatRes( listCatRes );
+    while ( TKey * keyPropRes = ( TKey* )nextInListCatRes() ) {
+      if ( std::string( keyPropRes->GetClassName() ) != nameDirClass ) continue;
+      const std::string kinProp( keyPropRes->GetName() );
+      TDirectory * dirPropRes_( dynamic_cast< TDirectory* >( dirCatRes_->Get( kinProp.c_str() ) ) );
+
+      TList * listPropRes( dirPropRes_->GetListOfKeys() );
+      TIter nextInListPropRes( listPropRes );
+      bool bEta( false );
+      while ( TKey * keyEtaRes = ( TKey* )nextInListPropRes() ) {
+        if ( std::string( keyEtaRes->GetClassName() ) != nameDirClass ) continue;
+        const std::string binEta( keyEtaRes->GetName() );
+        TDirectory * dirEtaRes_( dynamic_cast< TDirectory* >( dirPropRes_->Get( binEta.c_str() ) ) );
+
+        const std::string nameEtaRes( "fitExist_" + objCat + "_" + kinProp + "_" + binEta );
+        const std::string nameEtaInvRes( "fitExist_" + objCat + "_Inv_" + kinProp + "_" + binEta );
+
+        TList * listEtaRes( dirEtaRes_->GetListOfKeys() );
+        TIter nextInListEtaRes( listEtaRes );
+        TF1 * resSigma( 0 );
+        TF1 * resSigmaInv( 0 );
+        while ( TKey * keyFunc = ( TKey* )nextInListEtaRes() ) {
+          if ( std::string( keyFunc->GetClassName() ) != nameFuncClass ) continue;
+          resSigma    = dynamic_cast< TF1* >( dirEtaRes_->Get( nameEtaRes.c_str() ) );
+          resSigmaInv = dynamic_cast< TF1* >( dirEtaRes_->Get( nameEtaInvRes.c_str() ) );
+        }
+        if ( ( resSigma && resSigmaInv ) || ( ! resSigma && ! resSigmaInv ) ) {
+          std::cout << argv[ 0 ] << " --> WARNING:" << std::endl
+                    << "    inconsistent resolution functions in '" << objCat << "', '" << kinProp << "', '" << binEta << std::endl;
+          continue;
+        }
+        if ( ! bEta ) {
+          nominalInv_.at( uCat ).push_back( resSigmaInv ? true : false );
+          bEta = true;
+        }
+
+      } // loop: nextInListPropRes()
+
+      if (! bEta  ) {
+        std::cout << argv[ 0 ] << " --> ERROR:" << std::endl
+                  << "    no resolution function in "; dirPropRes_->pwd();
+        returnStatus_ += 0x200;
+      }
+
+    } // loop: nextInListCatRes()
+
+  } // loop: uCat < objCats_.size()
+
 
   std::vector< TFile* > files_;
 
@@ -80,10 +172,10 @@ int main(  int argc, char * argv[] )
 
   // Open input files
 
-  bool first( true );
+//   bool first( true );
   for ( unsigned uFile = 0; uFile < inFiles_.size(); ++uFile ) {
     const std::string inFile( inFiles_.at( uFile ) );
-    TFile * file( TFile::Open( inFile.c_str() ) );
+    TFile * file( TFile::Open( inFile.c_str(), "READ" ) );
     if ( ! file ) {
       std::cout << argv[ 0 ] << " --> ERROR:" << std::endl
                 << "   input file '" << inFiles_.at( uFile ) << "' missing; trying next file" << std::endl;
@@ -92,45 +184,45 @@ int main(  int argc, char * argv[] )
     }
     files_.push_back( file );
 
-    // Get binning (onyl once; has to be identical for all input files)
-    if ( first ) {
-      if ( verbose_ )
-        std::cout << std::endl
-                  << argv[ 0 ] << " --> INFO:" << std::endl
-                  << "   extract binning from input file '" << inFiles_.at( uFile ) << "'" << std::endl;
-      TDirectory * dirSel = ( TDirectory* )( file->Get( evtSel_.c_str() ) );
-
-      for ( unsigned uCat = 0; uCat < objCats_.size(); ++uCat ) {
-        const std::string objCat( objCats_.at( uCat ) );
-        TDirectory * dirCat( ( TDirectory* )( dirSel->Get( objCat.c_str() ) ) );
-        if ( ! dirCat ) {
-          std::cout << argv[ 0 ] << " --> WARNING:" << std::endl
-                    << "   object category '" << objCat << "' does not exist in input file '" << inFile << "'" << std::endl;
-          continue;
-        }
-
-        // Eta binning
-        std::vector< double > etaBins;
-        TH1D * histEtaBins( ( TH1D* )( dirCat->Get( std::string( objCat + "_binsEta" ).c_str() ) ) );
-        for ( int iEta = 1; iEta <= histEtaBins->GetNbinsX(); ++iEta ) {
-          etaBins.push_back( histEtaBins->GetBinLowEdge( iEta ) );
-        }
-        etaBins.push_back( histEtaBins->GetBinLowEdge( histEtaBins->GetNbinsX() ) + histEtaBins->GetBinWidth( histEtaBins->GetNbinsX() ) );
-        etaBins_.push_back( etaBins );
-
-        // Pt binning
-        std::vector< double > ptBins;
-        TH1D * histPtBins( ( TH1D* )( dirCat->Get( std::string( objCat + "_binsPt" ).c_str() ) ) );
-        for ( int iPt = 1; iPt <= histPtBins->GetNbinsX(); ++iPt ) {
-          ptBins.push_back( histPtBins->GetBinLowEdge( iPt ) );
-        }
-        ptBins.push_back( histPtBins->GetBinLowEdge( histPtBins->GetNbinsX() ) + histPtBins->GetBinWidth( histPtBins->GetNbinsX() ) );
-        ptBins_.push_back( ptBins );
-      }
-
-      first = false;
-    }
-  }
+//     // Get binning (onyl once; has to be identical for all input files)
+//     if ( first ) {
+//       if ( verbose_ )
+//         std::cout << std::endl
+//                   << argv[ 0 ] << " --> INFO:" << std::endl
+//                   << "   extract binning from input file '" << inFiles_.at( uFile ) << "'" << std::endl;
+//       TDirectory * dirSel_ = ( TDirectory* )( file->Get( evtSel_.c_str() ) );
+//
+//       for ( unsigned uCat = 0; uCat < objCats_.size(); ++uCat ) {
+//         const std::string objCat( objCats_.at( uCat ) );
+//         TDirectory * dirCat_( ( TDirectory* )( dirSel_->Get( objCat.c_str() ) ) );
+//         if ( ! dirCat_ ) {
+//           std::cout << argv[ 0 ] << " --> WARNING:" << std::endl
+//                     << "   object category '" << objCat << "' does not exist in input file '" << inFile << "'" << std::endl;
+//           continue;
+//         }
+//
+//         // Eta binning
+//         std::vector< double > etaBins;
+//         TH1D * histEtaBins( ( TH1D* )( dirCat_->Get( std::string( objCat + "_binsEta" ).c_str() ) ) );
+//         for ( int iEta = 1; iEta <= histEtaBins->GetNbinsX(); ++iEta ) {
+//           etaBins.push_back( histEtaBins->GetBinLowEdge( iEta ) );
+//         }
+//         etaBins.push_back( histEtaBins->GetBinLowEdge( histEtaBins->GetNbinsX() ) + histEtaBins->GetBinWidth( histEtaBins->GetNbinsX() ) );
+//         etaBins_.push_back( etaBins );
+//
+//         // Pt binning
+//         std::vector< double > ptBins;
+//         TH1D * histPtBins( ( TH1D* )( dirCat_->Get( std::string( objCat + "_binsPt" ).c_str() ) ) );
+//         for ( int iPt = 1; iPt <= histPtBins->GetNbinsX(); ++iPt ) {
+//           ptBins.push_back( histPtBins->GetBinLowEdge( iPt ) );
+//         }
+//         ptBins.push_back( histPtBins->GetBinLowEdge( histPtBins->GetNbinsX() ) + histPtBins->GetBinWidth( histPtBins->GetNbinsX() ) );
+//         ptBins_.push_back( ptBins );
+//       }
+//
+//       first = false;
+//     }
+  }  // loop: uFile
   if ( files_.empty() ) {
       std::cout << argv[ 0 ] << " --> ERROR:" << std::endl
                 << "   no input files found" << std::endl;
@@ -139,8 +231,8 @@ int main(  int argc, char * argv[] )
   }
 
   TFile * refFile( files_.at( 0 ) );
-  TDirectory * dirSel = ( TDirectory* )( refFile->Get( evtSel_.c_str() ) );
-  if ( ! dirSel ) {
+  TDirectory * dirSel_ = ( TDirectory* )( refFile->Get( evtSel_.c_str() ) );
+  if ( ! dirSel_ ) {
     std::cout << argv[ 0 ] << " --> ERROR:" << std::endl
               << "   selection '" << evtSel_ << "' does not exist in reference file '" << refFile->GetName() << "'" << std::endl;
     returnStatus_ += 0x20;
@@ -150,83 +242,125 @@ int main(  int argc, char * argv[] )
   // Loop over configured object categories
   for ( unsigned uCat = 0; uCat < objCats_.size(); ++uCat ) {
     const std::string objCat( objCats_.at( uCat ) );
-    TDirectory * dirCat( ( TDirectory* )( dirSel->Get( objCat.c_str() ) ) );
-    if ( ! dirCat ) {
+    TDirectory * dirCat_( ( TDirectory* )( dirSel_->Get( objCat.c_str() ) ) );
+    if ( ! dirCat_ ) {
       std::cout << argv[ 0 ] << " --> WARNING:" << std::endl
                 << "   object category '" << objCat << "' does not exist in reference file '" << refFile->GetName() << "'" << std::endl;
       continue;
     }
 
+    TDirectory * dirCatRes_( dynamic_cast< TDirectory* >( resolutionFile->Get( objCat.c_str() ) ) );
+    if ( ! dirCatRes_ ) {
+      std::cout << argv[ 0 ] << " --> WARNING:" << std::endl
+                << "    object category '" << objCat << "' does not exist in resolution file" << std::endl;
+      continue;
+    }
+
     // Loop over kinematic properties
-    TList * listCat( dirCat->GetListOfKeys() );
+    TList * listCat( dirCat_->GetListOfKeys() );
     TIter nextInListCat( listCat );
+    unsigned uProp( 0 );
     while ( TKey * keyProp = ( TKey* )nextInListCat() ) {
-      if ( std::string( keyProp->GetClassName() ) != dirClassName_ ) continue;
+      if ( std::string( keyProp->GetClassName() ) != nameDirClass ) continue;
+      if ( onlyExisting_ && ! ( uProp < nominalInv_.at( uCat ).size() ) ) break;
       const std::string kinProp( keyProp->GetName() );
-      TDirectory * dirProp( ( TDirectory* )( dirCat->Get( kinProp.c_str() ) ) );
+      TDirectory * dirProp_( ( TDirectory* )( dirCat_->Get( kinProp.c_str() ) ) );
+
+      TDirectory * dirPropRes_( ( TDirectory* )( dirCatRes_->Get( kinProp.c_str() ) ) );
 
       // Loop over fit versions
-      TList * listProp( dirProp->GetListOfKeys() );
+      TList * listProp( dirProp_->GetListOfKeys() );
       TIter nextInListProp( listProp );
       while ( TKey * keyFit = ( TKey* )nextInListProp() ) {
-        if ( std::string( keyFit->GetClassName() ) != dirClassName_ ) continue;
+        if ( std::string( keyFit->GetClassName() ) != nameDirClass ) continue;
         const std::string subFit( keyFit->GetName() );
-        TDirectory * dirFit = ( TDirectory* )( dirProp->Get( subFit.c_str() ) );
+        // These are real switches: depending on configuration, only one setting combination can be run at a time
+        if ( useAlt_  == ( subFit.find( "Alt" )  == std::string::npos ) ) continue;
+        if ( useSymm_ == ( subFit.find( "Symm" ) == std::string::npos ) ) continue;
+        if ( refGen_  == ( subFit.find( "Gen" )  == std::string::npos ) ) continue;
+        TDirectory * dirFit_ = ( TDirectory* )( dirProp_->Get( subFit.c_str() ) );
 
-        // Inversion flag from directory name
-        const bool inverse( subFit.substr( subFit.size() - 3 ) == "Inv" );
+        const std::string name( objCat + "_" + kinProp + "_" + subFit );
+
+        // Inversion flag from directory nameEta
+        const bool inverse( subFit.find( "Inv" ) != std::string::npos );
+        if ( onlyExisting_ && nominalInv_.at( uCat ).at( uProp ) != inverse ) continue;
 
         // Loop over eta bins
-        TList * listFit( dirFit->GetListOfKeys() );
+        TList * listFit( dirFit_->GetListOfKeys() );
         TIter nextInListFit( listFit );
         while ( TKey * keyEta = ( TKey* )nextInListFit() ) {
-          if ( std::string( keyEta->GetClassName() ) != dirClassName_ ) continue;
+          if ( std::string( keyEta->GetClassName() ) != nameDirClass ) continue;
           const std::string binEta( keyEta->GetName() );
-          const unsigned uEta( std::atoi( binEta.substr( 3 ).data() ) );
-          const std::string nameDir( evtSel_ + "/" + objCat + "/" + kinProp + "/" + subFit + "/" + binEta );
-          const std::string name( objCat + "_" + kinProp + "_" + subFit + "_" + binEta );
+          TDirectory * dirEta_( dynamic_cast< TDirectory* >( dirFit_->Get( binEta.c_str() ) ) );
+          if ( verbose_ ) dirEta_->pwd();
 
-          dirFit->cd( binEta.c_str() );
-          if ( verbose_ ) gDirectory->pwd();
-          const std::string nameSigma( name + "_Sigma" );
-          TH1D * histSigma( ( TH1D* )( gDirectory->Get( nameSigma.c_str() ) ) );
+          const std::string nameEta( name + "_" + binEta );
+          const std::string nameSigma( nameEta + "_Sigma" );
+          TH1D * histSigma( ( TH1D* )( dirEta_->Get( nameSigma.c_str() ) ) );
+
+          TDirectory * dirEtaRes_( dynamic_cast< TDirectory* >( dirPropRes_->Get( binEta.c_str() ) ) );
+          const std::string nameEtaExist( inverse ? "fitExist_" + objCat + "_Inv_" + kinProp + "_" + binEta  : "fitExist_" + objCat + "_" + kinProp + "_" + binEta );
+          TF1 * resSigma( 0 );
+          TList * listEtaRes( dirEtaRes_->GetListOfKeys() );
+          TIter nextInListEtaRes( listEtaRes );
+          while ( TKey * keyFuncRes = ( TKey* )nextInListEtaRes() ) {
+            if ( std::string( keyFuncRes->GetClassName() ) != nameFuncClass ) continue;
+            resSigma = dynamic_cast< TF1* >( dirEtaRes_->Get( nameEtaExist.c_str() ) );
+          }
+          if ( resSigma == 0 ) {
+            std::cout << argv[ 0 ] << " --> ERROR:" << std::endl
+                      << "    no resolution function in "; dirEtaRes_->pwd();
+          }
+
           if ( histSigma != 0 ) {
-            const std::string nameFitSigma( "fit_" + nameSigma );
-            TF1 * fitSigma( histSigma->GetFunction( nameFitSigma.c_str() ) );
-
             const std::string nameCanvSigma( "canv_" + nameSigma );
             TCanvas * canvSigma( new TCanvas( nameCanvSigma.c_str(), "", 768, 512 ) );
-            if ( fitSigma != 0 ) {
-              fitSigma->DrawCopy();
+            histSigma->Draw();
+            if ( resSigma != 0 ) {
+              resSigma->SetLineColor( kRed );
+              resSigma->Draw( "Same" );
             }
 
-            for ( unsigned uFile = 1; uFile < files_.size(); ++uFile ) { // skip 1st file (we have it already)
-              files_.at( uFile )->cd( nameDir.c_str() );
-              if ( verbose_ ) gDirectory->pwd();
-              canvSigma->cd();
-              TH1D * histSigmaFile( ( TH1D* )( gDirectory->Get( nameSigma.c_str() ) ) );
-              if ( histSigmaFile != 0 ) {
-                TF1 * fitSigmaFile( histSigmaFile->GetFunction( nameFitSigma.c_str() ) );
-                fitSigma->DrawCopy( "Same" );
-              }
-            }
+//             const std::string nameDirEta( evtSel_ + "/" + objCat + "/" + kinProp + "/" + subFit + "/" + binEta );
+//             for ( unsigned uFile = 1; uFile < files_.size(); ++uFile ) { // skip 1st file (we have it already)
+//               files_.at( uFile )->cd( nameDirEta.c_str() );
+//               if ( verbose_ ) gDirectory->pwd();
+//               canvSigma->cd();
+//               TH1D * histSigmaFile( ( TH1D* )( gDirectory->Get( nameSigma.c_str() ) ) );
+//               if ( histSigmaFile != 0 ) {
+//                 TF1 * fitSigmaFile( histSigmaFile->GetFunction( nameFitSigma.c_str() ) );
+//                 fitSigma->DrawCopy( "Same" );
+//               }
+//             }
 
+            canvSigma->Update();
             canvSigma->SaveAs( ".png" );
 
             delete canvSigma;
 
           }
+          else {
+            std::cout << argv[ 0 ] << " --> ERROR:" << std::endl
+                      << "   no resolution histogram in "; dirEta_->pwd();
+            returnStatus_ += 0x1000;
+            continue;
+          }
 
-        }
+        } // loop: keyEta
 
-      }
+      } // loop: keyFit
 
-    }
+      ++uProp;
+    } // loop: keyProp
 
-  }
+  } // loop: uCat
 
   // Close input files
   for ( unsigned uFile = 0; uFile < files_.size(); ++uFile ) files_.at( uFile )->Close();
+
+  // Close resolution file
+  resolutionFile->Close();
 
   return returnStatus_;
 
--- src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/plotTopHitFitResolutionFunctions_cfg.py
+++ src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/plotTopHitFitResolutionFunctions_cfg.py
@@ -3,29 +3,98 @@ import os
 import FWCore.ParameterSet.Config as cms
 
 # Steering
+
 runTest = True
 
 # Origin of existing resolution functions
 # era = 'Spring10'
-era = 'Summer11'
+era    = 'Summer11'
+sample = 'Fall11_R4_1'
+
+# Settings
+overwrite = True # to throw away earlier versions of histograms, trees and functions
+# Exclusive switches:
+usePileUp = False
+useAlt    = False
+useSymm   = True
+refGen    = False
+refSel    = True
+if runTest:
+  refSel = False
+
+# I/O
+inputFile = 'fitTopHitFit_from%s_%s.root'%( era, sample )
+if usePileUp:
+  inputFile = inputFile.replace( '.root', '_PileUp.root' )
+logFile = inputFile.replace( 'root', 'log' )
+if not runTest:
+  if useAlt:
+    logFile = logFile.replace( '.', '_Alt.', 1 )
+  if useSymm:
+    logFile = logFile.replace( '.', '_Symm.', 1 )
+  if refGenJet:
+    logFile = logFile.replace( '.', '_GenJet.', 1 )
+  if refSel:
+    logFile = logFile.replace( '.', '_Ref.', 1 )
+inputFile  = 'file:%s/output/%s'%( os.getenv( "CMSSW_BASE" ), inputFile )
+logFile    = 'file:%s/output/%s'%( os.getenv( "CMSSW_BASE" ), logFile )
+logFile    = logFile.replace( 'fitTopHitFit', 'plotTopHitFitResolutionFunctions' )
+outputFile = logFile.replace( 'log', 'root' )
+
+
+# Processing
 
 process = cms.PSet()
 process.verbose = cms.bool( False )
 if runTest:
   process.verbose = True
-process.plotter = cms.PSet(
-  outputFile = cms.string( 'file:%s/output/plotHitFitResolutionFunctions_from%s.root'%( os.getenv( "CMSSW_BASE" ), era ) )
-, inputFiles = cms.vstring( 'file:%s/output/fitTopHitFit_from%s.root'%( os.getenv( "CMSSW_BASE" ), era )
-                          )
-, objectCategories = cms.vstring( 'Mu'
-, objectCategories = cms.vstring( 'UdscJet'
-                                )
-, selection = cms.string( 'analyzeHitFitResolutionFunctions' )
+process.objectCategories = cms.vstring( 'Mu'
+                                      , 'UdscJet'
+                                      )
+if not runTest:
+  process.objectCategories.append( 'Elec' )
+  process.objectCategories.append( 'BJet' )
+  process.objectCategories.append( 'MET' )
+process.overwrite = cms.bool( overwrite )
+process.usePileUp = cms.bool( usePileUp )
+process.useAlt    = cms.bool( useAlt )
+process.useSymm   = cms.bool( useSymm )
+process.refGen    = cms.bool( refGen )
+process.refSel    = cms.bool( refSel )
+
+process.io = cms.PSet(
+  inputFiles     = cms.vstring( inputFile )
+, sample         = cms.string( sample )
+, outputFile     = cms.string( outputFile )
+, resolutionFile = cms.string( 'file:%s/output/existingHitFitResolutionFunctions_%s.root'%( os.getenv( "CMSSW_BASE" ), era ) )
 )
-if runTest:
-  process.plotter.outputFile = 'file:%s/output/plotHitFitResolutionFunctions_from%s.test.root'%( os.getenv( "CMSSW_BASE" ), era )
+
+process.plot = cms.PSet(
+  onlyExisting = cms.bool( True ) # True includes the possibility of writing resolution function text files.
+, writeFiles   = cms.bool( True ) # True takes effect only, if "onlyExisting" is True, too.
+)
+
+
+# Messaging
+
+print
+print 'Input files:'
+print '------------'
+for inputFile in process.io.inputFiles:
+  print inputFile
+if overwrite:
+  print
+  print 'WARNING: existing resolution function plots will be overwritten!'
 else:
-  process.plotter.objectCategories.append( 'Elec' )
-  process.plotter.objectCategories.append( 'BJet' )
-  process.plotter.objectCategories.append( 'MET' )
-  #process.plotter.selection = 'analyzeHitFitResolutionFunctions_Reference'
+  print
+  print 'INFO: existing resolution function plots will be kept.'
+print
+print 'ROOT output file destination:'
+print '---------------------'
+print outputFile
+print
+print 'Log file destination:'
+print '---------------------'
+print logFile
+print '================================================================================'
+print
