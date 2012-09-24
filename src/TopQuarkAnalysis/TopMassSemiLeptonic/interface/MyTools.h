81294ce1d81e1fd8805e4842f94f3888d4d22f89
vadler <>
2012-09-24 11:18:25 +0000

    Start optimisation of starting values for transfer functions.

--- src/TopQuarkAnalysis/TopMassSemiLeptonic/interface/MyTools.h
+++ src/TopQuarkAnalysis/TopMassSemiLeptonic/interface/MyTools.h
@@ -8,9 +8,10 @@
 
 namespace my {
 
-  void setParametersFitFrac( TF1 * fit, TH1D * histo, bool useBkgFunction );
+  void setParametersFitFrac( TF1 * fit, TH1D * histo, bool useBkgFunction = false );
   void setParametersFitDelta( TF1 * fit, TH1D * histo );
-  void setParametersFitTransfer1D( TF1 * fit, TH1D * histo );
+  void setParametersFitTransfer1D( TF1 * fit, TH1D * histo, bool useBkgFunction = false );
+  void setParametersFitTransfer2D( TF2 * fit, TH2D * histo, bool useBkgFunction = false );
   void setParametersBkg( TF1 * fit, TF1 * bkg );
 
 }
