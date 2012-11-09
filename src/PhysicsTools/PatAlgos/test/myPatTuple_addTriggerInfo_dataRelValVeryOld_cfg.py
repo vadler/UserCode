c67eeaaf68ce3bd5ce9904947573c4e412aa8a78
vadler <>
2012-11-09 18:34:51 +0000

    Update for removed RelVals.

--- src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_dataRelValVeryOld_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_dataRelValVeryOld_cfg.py
@@ -2,7 +2,7 @@ import os
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
-cmsswVersion = 'CMSSW_5_2_5_cand1'
+cmsswVersion = 'CMSSW_5_2_7'
 globalTag    = 'GR_P_V39_AN1'
 process.GlobalTag.globaltag = '%s::All'%( globalTag )
 process.source.fileNames    = pickRelValInputFiles( #formerVersion = True
