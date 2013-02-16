df24c494f5a0e5e323b8cf30544d70cb257a8e9c
vadler <>
2013-02-16 19:49:50 +0000

    Adopt resolution function plotting.

--- src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/plotTopHitFitResolutionFunctions_fromSummer11_Fall11_R4_1_L3_unambiguousOnly_Symm_cfg.py
+++ src/TopQuarkAnalysis/TopMassSemiLeptonic/bin/plotTopHitFitResolutionFunctions_fromSummer11_Fall11_R4_1_L3_unambiguousOnly_Symm_cfg.py
@@ -18,7 +18,7 @@ process = cms.PSet(
         resolutionFile = cms.string('file:/afs/cern.ch/user/v/vadler/cms/Top/CMSSW_4_4_4/output/existingHitFitResolutionFunctions_Summer11.root'),
         outputFile = cms.string('file:/afs/cern.ch/user/v/vadler/cms/Top/CMSSW_4_4_4/output/plotTopHitFitResolutionFunctions_fromSummer11_Fall11_R4_1_L3_unambiguousOnly.root'),
         inputFiles = cms.vstring('file:/afs/cern.ch/user/v/vadler/cms/Top/CMSSW_4_4_4/output/fitTopHitFit_fromSummer11_Fall11_R4_1_L3_unambiguousOnly.root'),
-        pathPlots = cms.string('/afs/cern.ch/user/v/vadler/cms/Top/CMSSW_4_4_4/output/plots/fitTopHitFitResolutionFunctions/plotTopHitFitResolutionFunctions_fromSummer11_Fall11_R4_1_L3_unambiguousOnly_Ref_')
+        pathPlots = cms.string('/afs/cern.ch/user/v/vadler/cms/Top/CMSSW_4_4_4/output/plots/fitTopHitFitResolutionFunctions/plotTopHitFitResolutionFunctions_fromSummer11_Fall11_R4_1_L3_unambiguousOnly_')
     ),
     plot = cms.PSet(
         accuEvery = cms.uint32(5),
