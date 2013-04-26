492dacfafbbc82f641f191ff4c0416979c24527a
vadler <>
2013-04-26 18:20:08 +0000

    Python tools set-up for unscheduled.

--- src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_mcRelValFormer_cfg.py
+++ src/PhysicsTools/PatAlgos/test/myPatTuple_addTriggerInfo_mcRelValFormer_cfg.py
@@ -6,65 +6,60 @@ cmsswVersion = 'CMSSW_6_2_0_pre4'
 globalTag    = 'PRE_ST61_V1'
 
 ## Options
-process.options.wantSummary      = False
-process.options.allowUnscheduled = cms.untracked.bool( False )
-
-## Messaging
-#process.Tracer = cms.Service( "Tracer" )
+process.options.wantSummary      = True
+process.options.allowUnscheduled = cms.untracked.bool( True )
 
 ## Input
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
-process.source.fileNames    = pickRelValInputFiles( cmsswVersion  = cmsswVersion
-                                                  , relVal        = 'RelValProdTTbar'
-                                                  , dataTier      = 'AODSIM'
-                                                  , globalTag     = globalTag
-                                                  , maxVersions   = 1
-                                                  )
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
+#process.source.fileNames    = pickRelValInputFiles( cmsswVersion  = cmsswVersion
+                                                  #, relVal        = 'RelValProdTTbar'
+                                                  #, dataTier      = 'AODSIM'
+                                                  #, globalTag     = globalTag
+                                                  #, maxVersions   = 1
+                                                  #)
+process.source.fileNames = cms.untracked.vstring( '/store/relval/CMSSW_6_2_0_pre4/RelValProdTTbar/AODSIM/PRE_ST61_V1-v1/00000/E2A9A526-FB90-E211-AA90-003048F1748A.root' )
+process.maxEvents.input = 100
 process.source.skipBadFiles = cms.untracked.bool( True )
 
+# Messaging
+if process.maxEvents.input.value() <= 1:
+    process.Tracer = cms.Service( "Tracer" )
+
 ## Output
-process.out.fileName = '%s/output/myPatTuple_addTriggerInfo_mcRelValFormer.root'%( os.getenv( "CMSSW_BASE" ) )
+process.out.fileName = '%s/output/myPatTupleUnscheduled_addTriggerInfo_mcRelValFormer.root'%( os.getenv( "CMSSW_BASE" ) )
+process.out.outputCommands = cms.untracked.vstring('drop *' )
 process.out.outputCommands.append( 'keep edmTriggerResults_TriggerResults_*_*' )
 process.out.outputCommands.append( 'keep *_hltTriggerSummaryAOD_*_*' )
 
 ## Processing
-process.load( "PhysicsTools.PatAlgos.patSequences_cff" )
-process.p = cms.Path(
-  process.patDefaultSequence
-)
+process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
+process.load("PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff")
+#process.p = cms.Path(
+  #process.selectedPatCandidateSummary
+#)
 
 # Trigger
 from PhysicsTools.PatAlgos.tools.trigTools import *
-from PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff import *
-process.hallo                     = patTrigger.clone()
-process.hallo.addL1Algos          = cms.bool( True )
-process.hallo.l1ExtraMu           = cms.InputTag( 'l1extraParticles' )
-process.hallo.saveL1Refs          = cms.bool( True )
-process.hallo.addPathModuleLabels = cms.bool( True )
-process.tschuess           = patTriggerEvent.clone()
-process.tschuess.condGtTag = cms.InputTag( 'conditionsInEdm' )
-process.tschuess.l1GtTag   = cms.InputTag( 'gtDigis' )
-process.moin = somePatMuonTriggerMatchPDSingleMu.clone()
-process.tach = somePatMetTriggerMatchHLTMu17.clone()
+#switchOnTrigger( process, path = 'p' )
 switchOnTrigger( process )
 switchOnTriggerMatching( process )
 switchOnTriggerStandAlone( process )
 switchOnTriggerMatchingStandAlone( process )
-#switchOnTriggerMatchEmbedding( process )
+switchOnTriggerMatchEmbedding( process )
+from PhysicsTools.PatAlgos.triggerLayer1.triggerMatcherExamples_cfi import *
+process.moin = somePatMuonTriggerMatchPDSingleMu.clone()
+process.tach = somePatMetTriggerMatchHLTMu17.clone()
+switchOnTrigger( process, triggerProducer = 'hallo', triggerEventProducer = 'tschuess', hltProcess = '*' )
+process.hallo.addL1Algos          = cms.bool( True )
+process.hallo.l1ExtraMu           = cms.InputTag( 'l1extraParticles' )
+process.hallo.saveL1Refs          = cms.bool( True )
+process.hallo.addPathModuleLabels = cms.bool( True )
+process.tschuess.condGtTag = cms.InputTag( 'conditionsInEdm' )
+process.tschuess.l1GtTag   = cms.InputTag( 'gtDigis' )
 switchOnTrigger( process, triggerProducer = 'hallo', triggerEventProducer = 'tschuess', hltProcess = '*' )
 switchOnTriggerMatching( process, triggerMatchers = [ 'moin', 'tach' ], triggerProducer = 'hallo', triggerEventProducer = 'tschuess', hltProcess = '*' )
 switchOnTriggerStandAlone( process, triggerProducer = 'hallo', hltProcess = '*' )
 switchOnTriggerMatchingStandAlone( process, triggerMatchers = [ 'moin', 'tach' ], triggerProducer = 'hallo', hltProcess = '*' )
 switchOnTriggerMatchEmbedding( process, triggerMatchers = [ 'moin', 'tach' ], triggerProducer = 'hallo', hltProcess = '*' )
-# print
-# print 'Path p'
-# print '--> %s'%( process.p )
-print
-print 'Sequence patDefaultSequenceTrigger'
-print '--> %s'%( process.patDefaultSequenceTrigger )
-print
-print 'Sequence patDefaultSequenceTriggerEvent'
-print '--> %s'%( process.patDefaultSequenceTriggerEvent )
-print
 print 'PoolOutputModule out.outputCommands'
 print '--> %s'%( process.out.outputCommands )
